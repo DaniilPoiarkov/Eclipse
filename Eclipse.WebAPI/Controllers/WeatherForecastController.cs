615d91435cb051c63bb194ab8fcb0a5ba35ba789
Daniil P <daniilpoyarkov@gmail.com>
2023-08-31 00:07:36 +0300

    Removed Authorize attribute

--- Eclipse.WebAPI/Controllers/WeatherForecastController.cs
+++ Eclipse.WebAPI/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ using Microsoft.Identity.Web.Resource;
 
 namespace Eclipse.WebAPI.Controllers;
 
-[Authorize]
+//[Authorize]
 [ApiController]
 [Route("[controller]")]
 //[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
