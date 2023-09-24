01efd720c7a48e91fa26b68b0a0e3c0e4b40d24e
Daniil P <daniilpoyarkov@gmail.com>
2023-09-24 14:26:15 +0300

    Formatted code

--- Eclipse.Pipelines/Pipelines/MainMenu/Language/ChangeLanguagePipeline.cs
+++ Eclipse.Pipelines/Pipelines/MainMenu/Language/ChangeLanguagePipeline.cs
@@ -22,6 +22,17 @@ internal class ChangeLanguagePipeline : EclipsePipelineBase
         RegisterStage(SetLanguage);
     }
 
+    private IResult SendAvailableLanguages(MessageContext context)
+    {
+        var buttons = new List<InlineKeyboardButton>
+        {
+            InlineKeyboardButton.WithCallbackData(Localizer["Pipelines:Language:English"], "en"),
+            InlineKeyboardButton.WithCallbackData(Localizer["Pipelines:Language:Ukrainian"], "uk"),
+        };
+
+        return Menu(buttons, Localizer["Pipelines:Language:Choose"]);
+    }
+
     private IResult SetLanguage(MessageContext context)
     {
         if (!SupportedLanguage(context))
@@ -43,17 +54,4 @@ internal class ChangeLanguagePipeline : EclipsePipelineBase
             return context.Value.Equals("en") || context.Value.Equals("uk");
         }
     }
-
-    private IResult SendAvailableLanguages(MessageContext context)
-    {
-        var buttons = new List<InlineKeyboardButton>
-        {
-            InlineKeyboardButton.WithCallbackData(Localizer["Pipelines:Language:English"], "en"),
-            InlineKeyboardButton.WithCallbackData(Localizer["Pipelines:Language:Ukrainian"], "uk"),
-        };
-
-        return Menu(buttons, Localizer["Pipelines:Language:Choose"]);
-    }
-
-    
 }
