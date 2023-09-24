b12df0ea917aa9247483c1f5b9ed026f238fbf64
Daniil P <daniilpoyarkov@gmail.com>
2023-09-24 19:16:46 +0300

    Added core tests

---
+++ Eclipse.Core.Tests/PipelineProviderTests.cs
@@ -0,0 +1,38 @@
+﻿using Eclipse.Core.Core;
+using Eclipse.Core.Models;
+using Eclipse.Core.Pipelines;
+using Eclipse.Core.Tests.Pipelines;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using NSubstitute;
+
+namespace Eclipse.Core.Tests;
+
+public class PipelineProviderTests
+{
+    private readonly IPipelineProvider _pipelineProvider;
+
+    public PipelineProviderTests()
+    {
+        var pipelines = new PipelineBase[] { new Test1Pipeline(), new Test2Pipeline() };
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<Test1Pipeline>()
+            .AddSingleton<Test2Pipeline>()
+            .BuildServiceProvider();
+
+        var currentUser = Substitute.For<ICurrentTelegramUser>();
+        currentUser.GetCurrentUser().Returns(new TelegramUser());
+
+        _pipelineProvider = new PipelineProvider(pipelines, serviceProvider, currentUser );
+    }
+
+    [Fact]
+    public void Get_WhenPipelineCanBeRetrieved_ThenPipelineReturned()
+    {
+        var pipeline = _pipelineProvider.Get("Test1");
+        pipeline.As<Test1Pipeline>().Should().NotBeNull();
+    }
+}
