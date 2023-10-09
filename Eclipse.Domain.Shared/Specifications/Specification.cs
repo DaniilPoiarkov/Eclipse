f32c2786a858434bd794e6d4c11d245aed7d8f2b
Daniil P <daniilpoyarkov@gmail.com>
2023-10-09 21:18:09 +0300

    Added specification concept

---
+++ Eclipse.Domain.Shared/Specifications/Specification.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Eclipse.Domain.Shared.Specifications;
+
+public abstract class Specification<T>
+{
+    public abstract Expression<Func<T, bool>> IsSatisfied();
+
+    public static implicit operator Expression<Func<T, bool>>(Specification<T> specification) => specification.IsSatisfied();
+
+    public static implicit operator Func<T, bool>(Specification<T> specification) => specification.IsSatisfied().Compile();
+}
