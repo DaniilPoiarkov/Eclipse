4b5a1f05213b1a3ff9065c0c50240c97452231d3
Daniil P <daniilpoyarkov@gmail.com>
2023-10-13 01:38:19 +0300

    Removed commented code

--- Eclipse.Pipelines/Pipelines/EclipsePipelineBase.cs
+++ Eclipse.Pipelines/Pipelines/EclipsePipelineBase.cs
@@ -8,32 +8,12 @@ namespace Eclipse.Pipelines.Pipelines;
 public abstract class EclipsePipelineBase : PipelineBase
 {
     protected IEclipseLocalizer Localizer { get; private set; } = null!;
-    //{
-    //    get
-    //    {
-    //        var currentUser = GetService<ICurrentTelegramUser>().GetCurrentUser();
-    //        var localizer = GetService<IEclipseLocalizer>();
-            
-    //        if (currentUser is not null)
-    //        {
-    //            localizer.CheckCulture(currentUser.Id);
-    //        }
-
-    //        return localizer;
-    //    }
-    //}
 
     protected IReadOnlyCollection<IReadOnlyCollection<KeyboardButton>> MainMenuButtons => new List<KeyboardButton[]>
     {
         new[] { new KeyboardButton(Localizer["Menu:MainMenu:MyToDos"]), new KeyboardButton(Localizer["Menu:MainMenu:Reminders"]) },
         new[] { new KeyboardButton(Localizer["Menu:MainMenu:Suggest"]), new KeyboardButton(Localizer["Menu:MainMenu:Settings"]) }
     };
-
-    //private static TService GetService<TService>()
-    //    where TService : class
-    //{
-    //    return CachedServiceProvider.Services.GetRequiredService<TService>();
-    //}
 
     internal void SetLocalizer(IEclipseLocalizer localizer)
     {
