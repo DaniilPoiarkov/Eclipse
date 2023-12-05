024bce0ff442e4d02c489e69d7a3802654cbbd15
Daniil P <daniilpoyarkov@gmail.com>
2023-12-05 22:29:05 +0200

    Cleaned tests

--- test/Eclipse.Tests.Bdd/Domain/IdentityUsers/StepDefinitions/AddTodoItemStepDefinitions.cs
+++ test/Eclipse.Tests.Bdd/Domain/IdentityUsers/StepDefinitions/AddTodoItemStepDefinitions.cs
@@ -7,44 +7,29 @@ namespace Eclipse.Tests.Bdd.Domain.IdentityUsers.StepDefinitions;
 [Binding]
 public sealed class AddTodoItemStepDefinitions
 {
-    private readonly ScenarioContext _scenarioContext;
-
-    private static readonly string _userKey = "user";
-
-    public AddTodoItemStepDefinitions(ScenarioContext scenarioContext)
-    {
-        _scenarioContext = scenarioContext;
-    }
+    private readonly IdentityUser _user = IdentityUser.Create(Guid.NewGuid(), "Name", "Surname", "Username", 1);
 
     [Given("User with todo items")]
     public void GivenUserWithTodoItems(Table table)
     {
-        var user = IdentityUser.Create(Guid.NewGuid(), "Name", "Surname", "Username", 1);
-        
         var providedTodoItems = table.CreateSet<CreateTodoItem>();
 
         foreach (var item in providedTodoItems)
         {
-            user.AddTodoItem(item.Text);
+            _user.AddTodoItem(item.Text);
         }
-
-        _scenarioContext.Add(_userKey, user);
     }
 
     [When("Add todo item with following text: \"(.*)\"")]
     public void WhenAddTodoItemWithText(string text)
     {
-        var user = _scenarioContext.Get<IdentityUser>(_userKey);
-
-        user.AddTodoItem(text);
+        _user.AddTodoItem(text);
     }
 
     [Then("User must have (.*) todo items")]
     public void Then(int itemsCount)
     {
-        var user = _scenarioContext.Get<IdentityUser>(_userKey);
-        
-        user.TodoItems.Count.Should().Be(itemsCount);
+        _user.TodoItems.Count.Should().Be(itemsCount);
     }
 }
 
