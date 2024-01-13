8b0fffe830664908c5535f92caf9b9f6b375b541
Daniil P <daniilpoyarkov@gmail.com>
2024-01-14 00:56:53 +0200

    Removed useless job

--- Eclipse.Pipelines/Pipelines/MainMenu/Settings/SettingsPipelineBase.cs
+++ Eclipse.Pipelines/Pipelines/MainMenu/Settings/SettingsPipelineBase.cs
@@ -7,7 +7,7 @@ public abstract class SettingsPipelineBase : EclipsePipelineBase
     protected IEnumerable<IEnumerable<KeyboardButton>> SettingsMenuButtons => new List<IList<KeyboardButton>>
     {
         new[] { new KeyboardButton(Localizer["Menu:Settings:Language"]), new KeyboardButton(Localizer["Menu:Settings:SetGmt"]) },
-        //new[] { new KeyboardButton(Localizer["Menu:Settings:Notifications"]) },
+        new[] { new KeyboardButton(Localizer["Menu:Settings:Notifications"]) },
         new[] { new KeyboardButton(Localizer["Menu:MainMenu"]) }
     };
 }
