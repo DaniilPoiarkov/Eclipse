98db66f12abb9d76e6db565f495fa76b138f22ca
Daniil P <daniilpoyarkov@gmail.com>
2024-02-24 22:52:29 +0200

    docs + fix time parsing

--- Eclipse.Application/Extensions/StringExtensions.cs
+++ Eclipse.Application/Extensions/StringExtensions.cs
@@ -4,21 +4,49 @@ namespace System;
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// <code>
+    /// return source.Equals(other, StringComparison.CurrentCultureIgnoreCase);
+    /// </code>
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="other"></param>
+    /// <returns></returns>
     public static bool EqualsCurrentCultureIgnoreCase(this string source, string other)
     {
         return source.Equals(other, StringComparison.CurrentCultureIgnoreCase);
     }
 
+    /// <summary>
+    /// <code>
+    /// return string.IsNullOrEmpty(source);
+    /// </code>
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
     public static bool IsNullOrEmpty([NotNullWhen(false)] this string? source)
     {
         return string.IsNullOrEmpty(source);
     }
 
+    /// <summary>
+    /// <code>
+    /// return string.IsNullOrWhiteSpace(source);
+    /// </code>
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
     public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? source)
     {
         return string.IsNullOrWhiteSpace(source);
     }
 
+    /// <summary>
+    /// If string is null, empty or whitespaces, then returns empty string, otherwise applies formatting and returns result
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="format"></param>
+    /// <returns></returns>
     public static string FormattedOrEmpty(this string? str, Func<string, string> format)
     {
         if (str.IsNullOrEmpty() || str.IsNullOrWhiteSpace())
@@ -29,6 +57,15 @@ public static class StringExtensions
         return format(str);
     }
 
+    /// <summary>
+    /// Tries parse string value as <a cref="TimeOnly"></a>.
+    /// <para>
+    /// Returns <c>true</c> if parsed successfully (e.g. "12:30"), otherwise <c>false</c>
+    /// </para>
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="parsed"></param>
+    /// <returns></returns>
     public static bool TryParseAsTimeOnly(this string? str, out TimeOnly parsed)
     {
         parsed = default;
@@ -38,7 +75,9 @@ public static class StringExtensions
             return false;
         }
 
-        var values = str.Split(':').ToArray();
+        var values = str
+            .Split(':', StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
 
         if (values.Length != 2)
         {
@@ -50,9 +89,15 @@ public static class StringExtensions
             return false;
         }
 
-        parsed = new TimeOnly(hours, minutes);
-
-        return true;
+        try
+        {
+            parsed = new TimeOnly(hours, minutes);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
 
         static bool TryParse(string value, out int num) => int.TryParse(value, out num);
     }
