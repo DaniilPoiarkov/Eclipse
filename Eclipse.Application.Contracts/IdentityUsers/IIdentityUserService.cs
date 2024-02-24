bffcf900a3b4e71c17cfae0a228709d25c25715e
Daniil P <daniilpoyarkov@gmail.com>
2024-02-24 23:07:41 +0200

    Splitted user service interface

--- Eclipse.Application.Contracts/IdentityUsers/IIdentityUserService.cs
+++ Eclipse.Application.Contracts/IdentityUsers/IIdentityUserService.cs
@@ -1,16 +1,27 @@
 ﻿namespace Eclipse.Application.Contracts.IdentityUsers;
 
-public interface IIdentityUserService
+public interface IIdentityUserService : IIdentityUserReadService, IIdentityUserCreateUpdateService, IIdentityUserLogicService
+{
+
+}
+
+public interface IIdentityUserReadService
 {
     Task<IReadOnlyList<IdentityUserDto>> GetAllAsync(CancellationToken cancellationToken = default);
-
-    Task<IdentityUserDto> CreateAsync(IdentityUserCreateDto createDto, CancellationToken cancellationToken = default);
-
-    Task<IdentityUserDto> UpdateAsync(Guid id, IdentityUserUpdateDto updateDto, CancellationToken cancellationToken = default);
 
     Task<IdentityUserDto> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
 
     Task<IdentityUserDto> GetByChatIdAsync(long chatId, CancellationToken cancellationToken = default);
+}
 
+public interface IIdentityUserCreateUpdateService
+{
+    Task<IdentityUserDto> CreateAsync(IdentityUserCreateDto createDto, CancellationToken cancellationToken = default);
+
+    Task<IdentityUserDto> UpdateAsync(Guid id, IdentityUserUpdateDto updateDto, CancellationToken cancellationToken = default);
+}
+
+public interface IIdentityUserLogicService
+{
     Task<IdentityUserDto> SetUserGmtTimeAsync(Guid id, TimeOnly currentUserTime, CancellationToken cancellationToken = default);
 }
