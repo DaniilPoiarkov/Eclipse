dd29aa4f90999a0e29cc47a5d93f4d33651723c9
Daniil P <daniilpoyarkov@gmail.com>
2024-02-27 14:05:30 +0200

    Added tests

--- Common/Eclipse.Common/Linq/PaginatedList.cs
+++ Common/Eclipse.Common/Linq/PaginatedList.cs
@@ -11,6 +11,16 @@ public sealed class PaginatedList<T>
 
     public int Count { get; }
 
+    private static readonly int _minPage = 1;
+
+    private static readonly int _minPageSize = 1;
+
+    /// <summary>
+    /// Initializes the <a cref="PaginatedList{T}"></a>
+    /// </summary>
+    /// <param name="items">The items after applying pagination</param>
+    /// <param name="pages">Total pages</param>
+    /// <param name="totalCount">Total items count</param>
     public PaginatedList(T[] items, int pages, int totalCount)
     {
         Items = items;
@@ -19,8 +29,30 @@ public sealed class PaginatedList<T>
         Count = items.Length;
     }
 
+    /// <summary>
+    /// Creates <a cref="PaginatedList{T}"></a>
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="page"></param>
+    /// <param name="pageSize"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException">
+    /// page is less than 1
+    /// or
+    /// pageSize is less than 1
+    /// </exception>
     public static PaginatedList<T> Create(IEnumerable<T> source, int page, int pageSize)
     {
+        if (page < _minPage)
+        {
+            throw new ArgumentException($"Page indexing must start from {_minPage} exclusively.", nameof(page));
+        }
+
+        if (pageSize < _minPageSize)
+        {
+            throw new ArgumentException($"Page size must be at least {_minPageSize} element.", nameof(pageSize));
+        }
+
         var itemsCount = source.TryGetNonEnumeratedCount(out var count)
             ? count
             : source.Count();
---
+++ test/Eclipse.Common.Tests/Pagination/PaginationListTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+
+using NSubstitute.ExceptionExtensions;
+
+namespace Eclipse.Common.Tests.Pagination;
+
+public class PaginationListTests
+{
+    [Fact]
+    public void ToPaginatedList_WhenCreatedWithProperValues_ThenDataPresentInListIsValid()
+    {
+        var totalCount = 105;
+        var page = 2;
+        var pageSize = 10;
+        var pagesCount = 11;
+        var lessThan = 20;
+        var geaterThan = 9;
+
+        var numbers = Enumerable
+            .Range(0, totalCount)
+            .ToPaginatedList(page, pageSize);
+
+        numbers.TotalCount.Should().Be(totalCount);
+        numbers.Count.Should().Be(pageSize);
+        numbers.Items.Count.Should().Be(pageSize);
+        numbers.Pages.Should().Be(pagesCount);
+        numbers.Items.All(x => x < lessThan && x > geaterThan).Should().BeTrue();
+    }
+
+    [Fact]
+    public void ToPaginatedList_WhenCreatedWithInvalidPage_ThenArgumentExceptionThrown()
+    {
+        var totalCount = 15;
+        var page = 0;
+        var pageSize = 10;
+
+        var action = () => Enumerable
+            .Range(0, totalCount)
+            .ToPaginatedList(page, pageSize);
+
+        action.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void ToPaginatedList_WhenCreatedWithInvalidPageSize_ThenArgumentExceptionThrown()
+    {
+        var totalCount = 10;
+        var page = 1;
+        var pageSize = 0;
+
+        var action = () => Enumerable
+            .Range(0, totalCount)
+            .ToPaginatedList(page, pageSize);
+        
+        action.Should().ThrowExactly<ArgumentException>();
+    }
+}
