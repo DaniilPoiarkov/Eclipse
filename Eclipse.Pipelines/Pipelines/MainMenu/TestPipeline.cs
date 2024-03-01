43e29ad90613b1907ef9605446ff5eaba902e7b6
Daniil P <daniilpoyarkov@gmail.com>
2024-03-01 19:37:04 +0200

    Naming + test pipeline

---
+++ Eclipse.Pipelines/Pipelines/MainMenu/TestPipeline.cs
@@ -0,0 +1,29 @@
+﻿using Eclipse.Common.EventBus;
+using Eclipse.Core.Attributes;
+using Eclipse.Domain.IdentityUsers.Events;
+using Eclipse.Pipelines.Attributes;
+
+namespace Eclipse.Pipelines.Pipelines.MainMenu;
+
+//[ComingSoonFeature]
+[Route("Test", "/test")]
+internal sealed class TestPipeline : EclipsePipelineBase
+{
+    private readonly IEventBus _eventBus;
+
+    public TestPipeline(IEventBus eventBus)
+    {
+        _eventBus = eventBus;
+    }
+
+    protected override void Initialize()
+    {
+        RegisterStage(context => Text("ONE MORE TIME"));
+
+        RegisterStage(async (context, token) =>
+        {
+            await _eventBus.Publish(new TestDomainEvent(), token);
+            return Text("OK");
+        });
+    }
+}
