0302006d4c8c6e7e83a2d35c9f84a10c98d4aaa4
Daniil P <daniilpoyarkov@gmail.com>
2024-03-01 19:47:48 +0200

    Marked store as sealed

--- Eclipse.Pipelines/Stores/Pipelines/PipelineStore.cs
+++ Eclipse.Pipelines/Stores/Pipelines/PipelineStore.cs
@@ -3,7 +3,7 @@ using Eclipse.Core.Pipelines;
 
 namespace Eclipse.Pipelines.Stores.Pipelines;
 
-internal class PipelineStore : StoreBase<PipelineBase, PipelineKey>, IPipelineStore
+internal sealed class PipelineStore : StoreBase<PipelineBase, PipelineKey>, IPipelineStore
 {
     public PipelineStore(ICacheService cacheService) : base(cacheService)
     {
