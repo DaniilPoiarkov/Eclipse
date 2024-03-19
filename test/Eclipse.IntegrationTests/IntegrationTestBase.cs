a1ac1904e20614c2b92e203177b44fd50c262218
Daniil P <daniilpoyarkov@gmail.com>
2024-03-19 22:03:37 +0200

    Infrastructure setup and implemented 2 tests

---
+++ test/Eclipse.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,38 @@
+﻿using Bogus;
+
+using Eclipse.WebAPI.Filters.Authorization;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Eclipse.IntegrationTests;
+
+public abstract class IntegrationTestBase : IClassFixture<TestWebAppFactory>, IDisposable
+{
+    protected readonly IServiceScope Scope;
+
+    protected readonly HttpClient Client;
+
+    protected readonly Faker Faker;
+
+    protected IntegrationTestBase(TestWebAppFactory factory)
+    {
+        Scope = factory.Services.CreateScope();
+        Client = factory.CreateClient();
+
+        var apiKeyOptions = Scope.ServiceProvider
+            .GetRequiredService<IOptions<ApiKeyAuthorizationOptions>>();
+
+        Client.DefaultRequestHeaders.Add("X-API-KEY", apiKeyOptions.Value.EclipseApiKey);
+
+        Faker = new Faker();
+    }
+
+    public void Dispose()
+    {
+        Scope.Dispose();
+        Client.Dispose();
+
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ test/Eclipse.IntegrationTests/TestWebAppFactory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using Testcontainers.CosmosDb;
+
+namespace Eclipse.IntegrationTests;
+
+public sealed class TestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly CosmosDbContainer _dbContainer = new CosmosDbBuilder()
+        .WithImage("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(ConfigureCosmosClient);
+    }
+
+    private void ConfigureCosmosClient(IServiceCollection services)
+    {
+        services.RemoveAll<CosmosClient>();
+
+        var clientOptions = new CosmosClientOptions
+        {
+            ConnectionMode = ConnectionMode.Gateway,
+            HttpClientFactory = () => _dbContainer.HttpClient,
+        };
+
+        services.AddSingleton(new CosmosClient(_dbContainer.GetConnectionString(), clientOptions));
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+}
