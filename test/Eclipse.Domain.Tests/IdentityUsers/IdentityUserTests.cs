014d9801dae09d34ddd0d2785d05b1a4c44697b5
Daniil P <daniilpoyarkov@gmail.com>
2024-04-16 00:28:20 +0300

    Refactored SetGmt tests

--- test/Eclipse.Domain.Tests/IdentityUsers/IdentityUserTests.cs
+++ test/Eclipse.Domain.Tests/IdentityUsers/IdentityUserTests.cs
@@ -20,35 +20,21 @@ public class IdentityUserTests
         _sut = IdentityUserGenerator.Generate(1).First();
     }
 
-    [Fact]
-    public void SetGmt_WhenGmtPlus3_ThenGmtPlus3Set()
+    [Theory]
+    [InlineData(3)]
+    [InlineData(-4)]
+    [InlineData(-10)]
+    [InlineData(11)]
+    public void SetGmt_WhenLocalTimeSpecified_ThenProperCalculationAccordingToUtcSet(int gmt)
     {
         var utc = DateTime.UtcNow;
 
-        var hour = utc.Hour + 3 > 23
-            ? utc.Hour - 21
-            : utc.Hour + 3;
+        var hour = utc.Hour + gmt > 23
+            ? utc.Hour - (24 - gmt)
+            : utc.Hour + gmt;
 
         var currentUserTime = new TimeOnly(hour, utc.Minute);
-        var expected = new TimeSpan(3, 0, 0);
-
-        _sut.SetGmt(currentUserTime);
-
-        _sut.Gmt.Should().Be(expected);
-    }
-
-    [Fact]
-    public void SetGmt_WhenGmtMinus4_ThenGmtMinus4Set()
-    {
-        var utc = DateTime.UtcNow;
-
-        var hour = utc.Hour - 4 < 0
-            ? utc.Hour + 20
-            : utc.Hour - 4;
-
-        var currentUserTime = new TimeOnly(hour, utc.Minute);
-
-        var expected = new TimeSpan(-4, 0, 0);
+        var expected = new TimeSpan(gmt, 0, 0);
 
         _sut.SetGmt(currentUserTime);
 
