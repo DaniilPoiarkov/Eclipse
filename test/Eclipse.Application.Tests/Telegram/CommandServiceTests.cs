a43c406c1084b86c2de61c7bbf43fcd14cc201a1
Daniil P <daniilpoyarkov@gmail.com>
2024-04-16 00:46:50 +0300

    Refactored command service tests

--- test/Eclipse.Application.Tests/Telegram/CommandServiceTests.cs
+++ test/Eclipse.Application.Tests/Telegram/CommandServiceTests.cs
@@ -13,7 +13,7 @@ using Xunit;
 namespace Eclipse.Application.Tests.Telegram;
 
 public sealed class CommandServiceTests
-{
+{    
     private readonly ICommandService _sut;
 
     private static readonly string _descriptionPrefix = "BotCommand";
@@ -24,15 +24,22 @@ public sealed class CommandServiceTests
         _sut = new CommandService(botClient);
     }
 
-    [Fact]
-    public async Task Add_WhenCommandIsEmpty_ThenFailureResultReturned()
+    [Theory]
+    [InlineData("", "description", "CommandMinLength")]
+    [InlineData("commandcommandcommandcommandcommand", "description", "CommandMaxLength")]
+    [InlineData("command", "", "DescriptionMinLength")]
+    [InlineData("command",
+        "DescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLength" +
+        "DescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLengthDescriptionMaxLength",
+        "DescriptionMaxLength")]
+    public async Task Add_WhenRequestInvalid_ThenProperFailureResultReturned(string command, string description, string errorCode)
     {
-        var expectedError = Error.Validation("Command.Add", $"{_descriptionPrefix}:CommandMinLength");
+        var expectedError = Error.Validation("Command.Add", $"{_descriptionPrefix}:{errorCode}");
 
         var request = new AddCommandRequest
         {
-            Command = string.Empty,
-            Description = "description"
+            Command = command,
+            Description = description
         };
 
         var result = await _sut.Add(request);
@@ -44,66 +51,7 @@ public sealed class CommandServiceTests
         error.Description.Should().Be(expectedError.Description);
     }
 
-    [Fact]
-    public async Task Add_WhenCommandExcedesMaxLength_ThenFailureResultReturned()
-    {
-        var expectedError = Error.Validation("Command.Add", $"{_descriptionPrefix}:CommandMaxLength");
-
-        var request = new AddCommandRequest
-        {
-            Command = new string('x', CommandConstants.CommandMaxLength + 1),
-            Description = "description"
-        };
-
-        var result = await _sut.Add(request);
-
-        result.IsSuccess.Should().BeFalse();
-
-        var error = result.Error;
-        error.Code.Should().Be(expectedError.Code);
-        error.Description.Should().Be(expectedError.Description);
-    }
-
-    [Fact]
-    public async Task Add_WhenDescriptionIsEmpty_ThenFailureResultReturned()
-    {
-        var expectedError = Error.Validation("Command.Add", $"{_descriptionPrefix}:DescriptionMinLength");
-
-        var request = new AddCommandRequest
-        {
-            Command = "command",
-            Description = string.Empty
-        };
-
-        var result = await _sut.Add(request);
-
-        result.IsSuccess.Should().BeFalse();
-
-        var error = result.Error;
-        error.Code.Should().Be(expectedError.Code);
-        error.Description.Should().Be(expectedError.Description);
-    }
-
-    [Fact]
-    public async Task Add_WhenDescriptionExcedesMaxLength_ThenFailureResultReturned()
-    {
-        var expectedError = Error.Validation("Command.Add", $"{_descriptionPrefix}:DescriptionMaxLength");
-
-        var request = new AddCommandRequest
-        {
-            Command = "command",
-            Description = new string('x', CommandConstants.DescriptionMaxLength + 1)
-        };
-
-        var result = await _sut.Add(request);
-
-        result.IsSuccess.Should().BeFalse();
-
-        var error = result.Error;
-        error.Code.Should().Be(expectedError.Code);
-        error.Description.Should().Be(expectedError.Description);
-    }
-
+    
     [Fact]
     public async Task Add_WhenRequestIsValid_ThenSuccessResultReturned()
     {
