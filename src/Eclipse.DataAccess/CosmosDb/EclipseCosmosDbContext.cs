279130b2b23d3a67a9c978a0940d7442eb0536da
Daniil P <daniilpoyarkov@gmail.com>
2024-04-18 21:35:56 +0300

    Added another context to use efcore

--- src/Eclipse.DataAccess/CosmosDb/EclipseCosmosDbContext.cs
+++ src/Eclipse.DataAccess/CosmosDb/EclipseCosmosDbContext.cs
@@ -2,37 +2,22 @@
 using Eclipse.Domain.IdentityUsers;
 
 using Microsoft.Azure.Cosmos;
-using Microsoft.EntityFrameworkCore;
 
 namespace Eclipse.DataAccess.EclipseCosmosDb;
 
-public sealed class EclipseCosmosDbContext : DbContext//CosmosDbContext
+public sealed class EclipseCosmosDbContext : CosmosDbContext
 {
-    //public IContainer<IdentityUser> IdentityUsers => Container<IdentityUser>($"{nameof(IdentityUser)}s");
+    public IContainer<IdentityUser> IdentityUsers => Container<IdentityUser>($"{nameof(IdentityUser)}s");
 
-    //public EclipseCosmosDbContext(CosmosClient client, CosmosDbContextOptions options)
-    //    : base(client, options) { }
+    public EclipseCosmosDbContext(CosmosClient client, CosmosDbContextOptions options)
+        : base(client, options) { }
 
-    public DbSet<IdentityUser> IdentityUsers { get; set; }
-
-    public EclipseCosmosDbContext(DbContextOptions<EclipseCosmosDbContext> options) : base(options)
+    public override async Task InitializeAsync(CancellationToken cancellationToken = default)
     {
-        
-    }
+        var response = await Client.CreateDatabaseIfNotExistsAsync(Options.DatabaseId, cancellationToken: cancellationToken);
 
-    //public override async Task InitializeAsync(CancellationToken cancellationToken = default)
-    //{
-    //    var response = await Client.CreateDatabaseIfNotExistsAsync(Options.DatabaseId, cancellationToken: cancellationToken);
-
-    //    await response.Database.CreateContainerIfNotExistsAsync(
-    //        new ContainerProperties($"{nameof(IdentityUser)}s", "/identityUsers"),
-    //        cancellationToken: cancellationToken);
-    //}
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(EclipseCosmosDbContext).Assembly);
+        await response.Database.CreateContainerIfNotExistsAsync(
+            new ContainerProperties($"{nameof(IdentityUser)}s", "/identityUsers"),
+            cancellationToken: cancellationToken);
     }
 }
