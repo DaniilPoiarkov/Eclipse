e2cf8e931d1f4d72289b941a8d7313cdc71c86a7
Daniil P <daniilpoyarkov@gmail.com>
2024-05-11 21:33:27 +0300

    Cleanup and refactoring

--- src/Eclipse.Pipelines/Pipelines/MainMenu/MainMenuPipeline.cs
+++ src/Eclipse.Pipelines/Pipelines/MainMenu/MainMenuPipeline.cs
@@ -5,10 +5,8 @@ namespace Eclipse.Pipelines.Pipelines.MainMenu;
 [Route("Menu:MainMenu", "/main_menu")]
 internal sealed class MainMenuPipeline : EclipsePipelineBase
 {
-    private static readonly string _message = "Pipelines:MainMenu";
-
     protected override void Initialize()
     {
-        RegisterStage(_ => Menu(MainMenuButtons, Localizer[_message]));
+        RegisterStage(_ => Menu(MainMenuButtons, Localizer["Pipelines:MainMenu"]));
     }
 }
