346518980d4e9128b3753bd67d283a4d324c9ecb
Daniil P <daniilpoyarkov@gmail.com>
2024-07-14 19:57:35 +0300

    Added more tests

--- test/Eclipse.Localization.Tests/ResourceProviderTests.cs
+++ test/Eclipse.Localization.Tests/ResourceProviderTests.cs
@@ -6,6 +6,8 @@ using FluentAssertions;
 
 using Microsoft.Extensions.Options;
 
+using Newtonsoft.Json;
+
 using Xunit;
 
 namespace Eclipse.Localization.Tests;
@@ -31,9 +33,65 @@ public sealed class ResourceProviderTests
     [Theory]
     [InlineData("empty.json")]
     [InlineData("invalid.json")]
-    public void AddJsonFile_WhenFileIsWrongFormat_ThenExceptionThrown(string file)
+    public void Get_WhenFileIsWrongFormat_ThenExceptionThrown(string file)
     {
         var action = () => Sut.Get("en", $"Resources/Invalid/{file}");
         action.Should().ThrowExactly<UnableToParseLocalizationResourceException>();
+    }
+
+    [Theory]
+    [InlineData("en", "en.json")]
+    [InlineData("en", "en-1.json")]
+    [InlineData("uk", "uk.json")]
+    [InlineData("uk", "uk-1.json")]
+    public void Get_WhenLocationProperlySpecified_ThenResourceReturned(string culture, string file)
+    {
+        var path = $"Resources/Valid/{file}";
+        var json = File.ReadAllText(Path.GetFullPath(path));
+
+        var expected = JsonConvert.DeserializeObject<LocalizationResource>(json);
+
+        var resource = Sut.Get(culture, path);
+        resource.Culture.Should().Be(culture);
+        resource.Texts.Count.Should().Be(expected!.Texts.Count);
+        resource.Texts.Except(expected.Texts).Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData("en", "Test", "Test")]
+    [InlineData("uk", "Test", "Тест")]
+    public void Get_WhenCultureSpecified_ThenReturnsResourceWithProperCulture(string culture, string key, string expected)
+    {
+        _builder.AddJsonFiles("Resources/Valid");
+
+        var resource = Sut.Get(culture);
+        resource.Culture.Should().Be(culture);
+
+        var value = resource.Texts[key];
+        value.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("Test", "en", "Test")]
+    [InlineData("Тест", "uk", "Test")]
+    [InlineData("Message {0}", "en", "Message{0}")]
+    public void GetWithValue_ThenResourceExist_ThenReturnsWithSpecifiedValue(string value, string expectedCulture, string expectedKey)
+    {
+        _builder.AddJsonFiles("Resources/Valid");
+
+        var resource = Sut.GetWithValue(value);
+
+        resource.Culture.Should().Be(expectedCulture);
+        var pair = resource.Texts.FirstOrDefault(pair => pair.Value == value);
+        pair.Key.Should().Be(expectedKey);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("localization test")]
+    public void GetWithValue_ThenResourceNotExist_THenExceptionThrown(string value)
+    {
+        var action = () => Sut.GetWithValue(value);
+        action.Should().ThrowExactly<LocalizationNotFoundException>();
     }
 }
