f652b4d94309eb728ba11b40d21997f664e1ee92
Daniil P <daniilpoyarkov@gmail.com>
2024-07-24 21:19:22 +0300

    Updated packages

--- src/Eclipse.Localization/Resources/LocalizationResource.cs
+++ src/Eclipse.Localization/Resources/LocalizationResource.cs
@@ -9,4 +9,11 @@ internal sealed class LocalizationResource
     public string Culture { get; set; } = string.Empty;
 
     public Dictionary<string, string> Texts { get; set; } = [];
+
+    public override string ToString()
+    {
+        return $"{nameof(Culture)}: {Culture};{Environment.NewLine}" +
+            $"{nameof(Texts)}:{Environment.NewLine}" +
+            $"{string.Join(", ", Texts.Select(pair => $"{pair.Key} - {pair.Value}"))}";
+    }
 }
