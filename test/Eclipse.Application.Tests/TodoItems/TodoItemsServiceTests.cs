59eac56566c67d3c5937b5b9d996e57c3ab48ac6
Daniil P <daniilpoyarkov@gmail.com>
2024-08-02 21:29:12 +0300

    Added tests

--- test/Eclipse.Application.Tests/TodoItems/TodoItemsServiceTests.cs
+++ test/Eclipse.Application.Tests/TodoItems/TodoItemsServiceTests.cs
@@ -7,6 +7,7 @@ using Eclipse.Domain.Shared.TodoItems;
 using Eclipse.Domain.TodoItems;
 using Eclipse.Domain.Users;
 using Eclipse.Tests.Generators;
+using Eclipse.Tests.Utils;
 
 using FluentAssertions;
 
@@ -36,7 +37,7 @@ public sealed class TodoItemsServiceTests
     [Fact]
     public async Task CreateAsync_WhenInputValid_ThenCreatedSuccessfully()
     {
-        var user = UserGenerator.Generate(1).First();
+        var user = UserGenerator.Get();
 
         _repository.GetByExpressionAsync(_ => true)
             .ReturnsForAnyArgs(Task.FromResult<IReadOnlyList<User>>([user]));
@@ -64,14 +65,7 @@ public sealed class TodoItemsServiceTests
     public async Task CreateAsync_WhenUserReachLimitOfItems_ThenFailureResultReturned()
     {
         var expectedError = UserDomainErrors.TodoItemsLimit(TodoItemConstants.Limit);
-        var user = UserGenerator.Generate(1).First();
-
-        var faker = new Faker();
-
-        for (int i = 0; i < 7; i++)
-        {
-            user.AddTodoItem(faker.Lorem.Word());
-        }
+        var user = CreateUser(7);
 
         _repository.GetByExpressionAsync(_ => true)
             .ReturnsForAnyArgs(Task.FromResult<IReadOnlyList<User>>([user]));
@@ -86,8 +80,7 @@ public sealed class TodoItemsServiceTests
         result.IsSuccess.Should().BeFalse();
 
         var error = result.Error;
-        error.Code.Should().Be(expectedError.Code);
-        error.Description.Should().Be(expectedError.Description);
+        ErrorComparer.AreEqual(error, expectedError);
         error.Args.Should().BeEquivalentTo(expectedError.Args);
     }
 
@@ -110,8 +103,7 @@ public sealed class TodoItemsServiceTests
         result.IsSuccess.Should().BeFalse();
         var error = result.Error;
 
-        error.Code.Should().Be(expectedError.Code);
-        error.Description.Should().Be(expectedError.Description);
+        ErrorComparer.AreEqual(expectedError, error);
         error.Args.Should().BeEquivalentTo(expectedError.Args);
     }
 
@@ -130,8 +122,77 @@ public sealed class TodoItemsServiceTests
         result.IsSuccess.Should().BeFalse();
         var error = result.Error;
 
-        error.Code.Should().Be(expectedError.Code);
-        error.Description.Should().Be(expectedError.Description);
+        ErrorComparer.AreEqual(expectedError, error);
         error.Args.Should().BeEquivalentTo(expectedError.Args);
+    }
+
+    [Theory]
+    [InlineData(3)]
+    [InlineData(1)]
+    [InlineData(6)]
+    public async Task GetListAsync_WhenUserHasTodoItems_ThenAllItemsReturned(int todoItemsCount)
+    {
+        var user = CreateUser(todoItemsCount);
+
+        _repository.FindAsync(user.Id)
+            .ReturnsForAnyArgs(Task.FromResult<User?>(user));
+
+        var result = await Sut.GetListAsync(user.Id);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Count.Should().Be(todoItemsCount);
+        result.Value.All(item => item.UserId == user.Id).Should().BeTrue();
+        result.Value.Select(item => IsValidTodoItem(item, user)).All(x => x).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task GetAsync_WhenTodoItemExists_ThenItReturned()
+    {
+        var user = CreateUser(1);
+
+        _repository.FindAsync(user.Id)
+            .ReturnsForAnyArgs(Task.FromResult<User?>(user));
+
+        var result = await Sut.GetAsync(user.Id, user.TodoItems.First().Id);
+
+        result.IsSuccess.Should().BeTrue();
+        IsValidTodoItem(result.Value, user).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task GetAsync_WhenTodoItemNotFound_ThenErrorReturned()
+    {
+        var expectedError = DefaultErrors.EntityNotFound(typeof(TodoItem));
+        var user = CreateUser(0);
+
+        _repository.GetByExpressionAsync(_ => true)
+            .ReturnsForAnyArgs(Task.FromResult<IReadOnlyList<User>>([user]));
+
+        var result = await Sut.GetAsync(user.Id, Guid.NewGuid());
+
+        result.IsSuccess.Should().BeFalse();
+        ErrorComparer.AreEqual(result.Error, expectedError);
+    }
+
+    private static bool IsValidTodoItem(TodoItemDto item, User user)
+    {
+        return user.GetTodoItem(item.Id) is TodoItem todoItem
+            && todoItem.Text == item.Text
+            && todoItem.UserId == user.Id
+            && todoItem.Id != Guid.Empty;
+    }
+
+    private static User CreateUser(int todoItemsCount)
+    {
+        var user = UserGenerator.Get();
+
+        var faker = new Faker();
+
+        for (int i = 0; i < todoItemsCount; i++)
+        {
+            user.AddTodoItem(faker.Lorem.Word());
+        }
+
+        return user;
     }
 }
