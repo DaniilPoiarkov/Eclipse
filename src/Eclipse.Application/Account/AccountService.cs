c6cb0ad840d085dd69b001cc19fc71fa8fe3f204
Daniil P <daniilpoyarkov@gmail.com>
2024-08-03 16:31:16 +0300

    Refactored account service, moved sign in code sending as background job

--- src/Eclipse.Application/Account/AccountService.cs
+++ src/Eclipse.Application/Account/AccountService.cs
@@ -1,13 +1,10 @@
-﻿using Eclipse.Application.Contracts.Account;
-using Eclipse.Application.Contracts.Telegram;
+﻿using Eclipse.Application.Account.Background;
+using Eclipse.Application.Contracts.Account;
+using Eclipse.Common.Background;
 using Eclipse.Common.Clock;
 using Eclipse.Common.Results;
 using Eclipse.Domain.Shared.Errors;
 using Eclipse.Domain.Users;
-using Eclipse.Localization.Culture;
-using Eclipse.Localization.Extensions;
-
-using Microsoft.Extensions.Localization;
 
 namespace Eclipse.Application.Account;
 
@@ -15,21 +12,15 @@ internal sealed class AccountService : IAccountService
 {
     private readonly UserManager _userManager;
 
-    private readonly ITelegramService _telegramService;
-
-    private readonly IStringLocalizer<AccountService> _stringLocalizer;
-
-    private readonly ICurrentCulture _currentCulture;
-
     private readonly ITimeProvider _timeProvider;
 
-    public AccountService(UserManager userManager, ITelegramService telegramService, IStringLocalizer<AccountService> stringLocalizer, ICurrentCulture currentCulture, ITimeProvider timeProvider)
+    private readonly IBackgroundJobManager _backgroundJobManager;
+
+    public AccountService(UserManager userManager, ITimeProvider timeProvider, IBackgroundJobManager backgroundJobManager)
     {
         _userManager = userManager;
-        _telegramService = telegramService;
-        _stringLocalizer = stringLocalizer;
-        _currentCulture = currentCulture;
         _timeProvider = timeProvider;
+        _backgroundJobManager = backgroundJobManager;
     }
 
     public async Task<Result> SendSignInCodeAsync(string userName, CancellationToken cancellationToken = default)
@@ -41,17 +32,17 @@ internal sealed class AccountService : IAccountService
             return DefaultErrors.EntityNotFound(typeof(User));
         }
 
-        using var _ = _currentCulture.UsingCulture(user.Culture);
-        _stringLocalizer.UseCurrentCulture(_currentCulture);
-
         user.SetSignInCode(_timeProvider.Now);
 
         await _userManager.UpdateAsync(user, cancellationToken);
 
-        return await _telegramService.Send(new SendMessageModel
+        await _backgroundJobManager.EnqueueAsync<SendSignInCodeBackgroundJob, SendSignInCodeArgs>(new SendSignInCodeArgs
         {
             ChatId = user.ChatId,
-            Message = _stringLocalizer["Account:{0}AuthenticationCode", user.SignInCode ?? string.Empty]
+            SignInCode = user.SignInCode,
+            Culture = user.Culture
         }, cancellationToken);
+
+        return Result.Success();
     }
 }
---
+++ src/Eclipse.Application/Account/Background/SendSignInCodeBackgroundJob.cs
@@ -0,0 +1,37 @@
+﻿using Eclipse.Application.Contracts.Telegram;
+using Eclipse.Common.Background;
+using Eclipse.Localization.Culture;
+using Eclipse.Localization.Extensions;
+
+using Microsoft.Extensions.Localization;
+
+namespace Eclipse.Application.Account.Background;
+
+internal sealed class SendSignInCodeBackgroundJob : IBackgroundJob<SendSignInCodeArgs>
+{
+    private readonly ICurrentCulture _currentCulture;
+
+    private readonly IStringLocalizer<SendSignInCodeBackgroundJob> _stringLocalizer;
+
+    private readonly ITelegramService _telegramService;
+
+    public SendSignInCodeBackgroundJob(ICurrentCulture currentCulture, IStringLocalizer<SendSignInCodeBackgroundJob> stringLocalizer, ITelegramService telegramService)
+    {
+        _currentCulture = currentCulture;
+        _stringLocalizer = stringLocalizer;
+        _telegramService = telegramService;
+    }
+
+    public Task ExecureAsync(SendSignInCodeArgs args, CancellationToken cancellationToken = default)
+    {
+        using var _ = _currentCulture.UsingCulture(args.Culture);
+
+        _stringLocalizer.UseCurrentCulture(_currentCulture);
+
+        return _telegramService.Send(new SendMessageModel
+        {
+            ChatId = args.ChatId,
+            Message = _stringLocalizer["Account:{0}AuthenticationCode", args.SignInCode]
+        }, cancellationToken);
+    }
+}
