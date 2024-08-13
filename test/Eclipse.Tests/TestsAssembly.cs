11ed863cda3551d8154ffc3e7cf37cfeed1dae1e
Daniil P <daniilpoyarkov@gmail.com>
2024-08-13 17:25:14 +0300

    Removed irrelevant files

--- test/Eclipse.Tests/TestsAssembly.cs
+++ test/Eclipse.Tests/TestsAssembly.cs
@@ -6,32 +6,10 @@ namespace Eclipse.Tests;
 
 public static class TestsAssembly
 {
-    private static readonly string _basePath = "Eclipse.Tests.Files";
-
     public static string ToBase64String(string value)
     {
         return Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
     }
-
-    public static MemoryStream GetValidUsersExcelFile()
-    {
-        return GetManifestResourceFile($"{_basePath}.valid-users.xlsx");
-    }
-
-    public static MemoryStream GetInvalidUsersExcelFile()
-    {
-        return GetManifestResourceFile($"{_basePath}.invalid-users.xlsx");
-    }
-
-    //public static MemoryStream GetValidTodoItemsExcelFile()
-    //{
-    //    return GetManifestResourceFile($"{_basePath}.valid-todo-items.xlsx");
-    //}
-
-    //public static MemoryStream GetValidRemindersExcelFile()
-    //{
-    //    return GetManifestResourceFile($"{_basePath}.valid-reminders.xlsx");
-    //}
 
     public static MemoryStream GetManifestResourceFile(string name)
     {
