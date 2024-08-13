3ca6ea9f32bc61a83f425c158a8ea550598a62c1
Daniil P <daniilpoyarkov@gmail.com>
2024-08-13 13:53:12 +0300

    Refactored import reminder tests

--- test/Eclipse.Application.Tests/Exporting/Reminders/ImortRemindersStrategyTests.cs
+++ test/Eclipse.Application.Tests/Exporting/Reminders/ImortRemindersStrategyTests.cs
@@ -2,13 +2,10 @@
 
 using Eclipse.Application.Exporting;
 using Eclipse.Application.Exporting.Reminders;
+using Eclipse.Common.Excel;
 using Eclipse.Domain.Users;
-using Eclipse.Infrastructure.Excel;
-using Eclipse.Tests;
 
 using FluentAssertions;
-
-using MiniExcelLibs;
 
 using NSubstitute;
 
@@ -18,6 +15,8 @@ namespace Eclipse.Application.Tests.Exporting.Reminders;
 
 public sealed class ImortRemindersStrategyTests
 {
+    private readonly IExcelManager _excelManager;
+
     private readonly IUserRepository _userRepository;
 
     private readonly IImportValidator<ImportReminderDto, ImportRemindersValidationOptions> _validator;
@@ -26,50 +25,72 @@ public sealed class ImortRemindersStrategyTests
 
     public ImortRemindersStrategyTests()
     {
+        _excelManager = Substitute.For<IExcelManager>();
         _userRepository = Substitute.For<IUserRepository>();
         _validator = Substitute.For<IImportValidator<ImportReminderDto, ImportRemindersValidationOptions>>();
-        _sut = new ImportRemindersStrategy(_userRepository, new ExcelManager(), _validator);
+        _sut = new ImportRemindersStrategy(_userRepository, _excelManager, _validator);
     }
 
     [Fact]
-    public async Task AddReminders_WhenFileIsValid_ThenProcessedSeccessfully()
+    public async Task ImportAsync_WhenRecordsValid_ThenImportedSuccessfully()
     {
-        using var ms = TestsAssembly.GetValidRemindersExcelFile();
+        using var ms = new MemoryStream();
 
-        var rows = ms.Query<ImportReminderDto>()
-            .Where(r => !r.UserId.IsEmpty());
+        IEnumerable<ImportReminderDto> rows = [
+            GetRow(),
+            GetRow(),
+        ];
 
         var faker = new Faker();
 
         var users = rows.Select(r => r.UserId)
-            .Distinct()
             .Select(id => User.Create(id, faker.Person.FirstName, faker.Person.LastName, faker.Person.UserName, faker.Random.Long(min: 0), false))
             .ToList();
 
-        _userRepository.GetByExpressionAsync(_ => true).ReturnsForAnyArgs(users);
+        _excelManager.Read<ImportReminderDto>(ms).Returns(rows);
         _validator.ValidateAndSetErrors(rows).ReturnsForAnyArgs(rows);
+        _userRepository.GetByExpressionAsync(_ => true).ReturnsForAnyArgs(users);
 
-        await _sut.ImportAsync(ms);
+        var result = await _sut.ImportAsync(ms);
 
-        await _userRepository.ReceivedWithAnyArgs().GetByExpressionAsync(_ => true);
-
+        result.IsSuccess.Should().BeTrue();
+        
         foreach (var user in users)
         {
+            user.Reminders.Should().HaveCount(1);
             await _userRepository.Received().UpdateAsync(user);
-            user.Reminders.IsNullOrEmpty().Should().BeFalse();
         }
     }
 
     [Fact]
-    public async Task AddReminders_WhenUserNotExist_ThenReportSend()
+    public async Task ImportAsync_WhenUserNotExist_ThenFailureResultReturned()
     {
-        using var ms = TestsAssembly.GetValidRemindersExcelFile();
+        using var ms = new MemoryStream();
 
+        var row = GetRow();
+
+        _excelManager.Read<ImportReminderDto>(ms).Returns([row]);
         _userRepository.GetByExpressionAsync(_ => true).ReturnsForAnyArgs([]);
+        _validator.ValidateAndSetErrors([row]).ReturnsForAnyArgs([row]);
 
-        await _sut.ImportAsync(ms);
+        var result = await _sut.ImportAsync(ms);
 
-        await _userRepository.ReceivedWithAnyArgs().GetByExpressionAsync(_ => true);
+        result.IsSuccess.Should().BeFalse();
+        row.Exception.Should().NotBeEmpty();
+
         await _userRepository.DidNotReceiveWithAnyArgs().UpdateAsync(default!);
+    }
+
+    private static ImportReminderDto GetRow()
+    {
+        var faker = new Faker();
+
+        return new ImportReminderDto
+        {
+            Id = Guid.NewGuid(),
+            NotifyAt = "12:00:00",
+            Text = faker.Lorem.Word(),
+            UserId = Guid.NewGuid()
+        };
     }
 }
