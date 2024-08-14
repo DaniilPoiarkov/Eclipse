3f40ee6b815c380a389cbe3adf9d3639d07dcae1
Daniil P <daniilpoyarkov@gmail.com>
2024-08-14 20:24:44 +0300

    Added string localizer builder with support to mock indexers and delegate calls from substitute to another instance

---
+++ test/Eclipse.Tests/CallHandlers/RedirectAllCallHandler.cs
@@ -0,0 +1,18 @@
+﻿using NSubstitute.Core;
+
+namespace Eclipse.Tests.CallHandlers;
+
+internal sealed class RedirectAllCallHandler<T> : ICallHandler
+{
+    private readonly T _instance;
+
+    public RedirectAllCallHandler(T instance)
+    {
+        _instance = instance;
+    }
+
+    public RouteAction Handle(ICall call)
+    {
+        return RouteAction.Return(call.GetMethodInfo().Invoke(_instance, call.GetArguments()));
+    }
+}
