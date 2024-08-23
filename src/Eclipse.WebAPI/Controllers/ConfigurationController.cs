2e97d16a83d487a413fa2c15765985f6ece3a35f
Daniil P <daniilpoyarkov@gmail.com>
2024-08-24 01:11:51 +0300

    Added configuration controller to obtain available cultures

--- src/Eclipse.WebAPI/Controllers/ConfigurationController.cs
+++ src/Eclipse.WebAPI/Controllers/ConfigurationController.cs
@@ -1,5 +1,7 @@
 ﻿using Eclipse.Pipelines.Options.Languages;
+
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
 
 namespace Eclipse.WebAPI.Controllers;
@@ -10,14 +12,22 @@ public class ConfigurationController : ControllerBase
 {
     private readonly IOptions<LanguageList> _languageListOptions;
 
-    public ConfigurationController(IOptions<LanguageList> languageListOptions)
+    private readonly IStringLocalizer<ConfigurationController> _stringLocalizer;
+
+    public ConfigurationController(IOptions<LanguageList> languageListOptions, IStringLocalizer<ConfigurationController> stringLocalizer)
     {
         _languageListOptions = languageListOptions;
+        _stringLocalizer = stringLocalizer;
     }
 
     [HttpGet("cultures")]
     public IActionResult GetCultures()
     {
-        return Ok(_languageListOptions.Value);
+        return Ok(_languageListOptions.Value
+            .Select(i => new LanguageInfo
+            {
+                Code = i.Code,
+                Language = _stringLocalizer[i.Language]
+            }));
     }
 }
