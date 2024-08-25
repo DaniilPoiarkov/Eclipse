ebc622d0fa79c153e8572b71e53efd2459cf6fc3
Daniil P <daniilpoyarkov@gmail.com>
2024-08-25 20:52:41 +0300

    Added configuration service test

---
+++ test/Eclipse.Application.Tests/Configuration/ConfigurationServiceTests.cs
@@ -0,0 +1,76 @@
+﻿using Eclipse.Application.Configuration;
+using Eclipse.Application.Contracts.Configuration;
+using Eclipse.Tests.Builders;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+
+using NSubstitute;
+
+using Xunit;
+
+namespace Eclipse.Application.Tests.Configuration;
+
+public sealed class ConfigurationServiceTests
+{
+    private readonly IStringLocalizer<ConfigurationService> _localizer;
+
+    private readonly IOptions<CultureList> _options;
+
+    private readonly ConfigurationService _sut;
+
+    public ConfigurationServiceTests()
+    {
+        _localizer = Substitute.For<IStringLocalizer<ConfigurationService>>();
+        _options = Substitute.For<IOptions<CultureList>>();
+        _sut = new(_options, _localizer);
+    }
+
+    [Fact]
+    public void GetCultures_WhenRequested_ThenReturnsLocalizedResponse()
+    {
+        var expected = new List<CultureInfo>()
+        {
+            new()
+            {
+                Code = "en",
+                Culture = "This is english"
+            },
+            new()
+            {
+                Code = "fr",
+                Culture = "This is franch"
+            },
+        };
+
+        var english = "English";
+        var franch = "Franch";
+
+        LocalizerBuilder<ConfigurationService>.Create(_localizer)
+            .For(english)
+                .Return("This is english")
+            .For(franch)
+                .Return("This is franch")
+            .Build();
+
+        _options.Value.Returns(
+        [
+            new()
+            {
+                Code = "en",
+                Culture = english
+            },
+            new()
+            {
+                Code = "fr",
+                Culture = franch
+            }
+        ]);
+
+        var result = _sut.GetCultures();
+
+        result.Should().BeEquivalentTo(expected);
+    }
+}
--- test/Eclipse.Tests/Builders/LocalizerBuilder.cs
+++ test/Eclipse.Tests/Builders/LocalizerBuilder.cs
@@ -7,19 +7,23 @@ namespace Eclipse.Tests.Builders;
 
 public class LocalizerBuilder<T> : ILocalizerIndexerSubstituteBuilder<T>, ILocalizerBuilder<T>
 {
-    private readonly IStringLocalizer<T> _localizer = Substitute.For<IStringLocalizer<T>>();
+    private readonly IStringLocalizer<T> _localizer;
 
     private string? _name;
 
     private object[]? _args;
 
-    private LocalizerBuilder() { }
+    private LocalizerBuilder(IStringLocalizer<T> localizer)
+    {
+        _localizer = localizer;
+    }
 
-    public static ILocalizerBuilder<T> Create() => new LocalizerBuilder<T>();
+    public static ILocalizerBuilder<T> Create() => new LocalizerBuilder<T>(Substitute.For<IStringLocalizer<T>>());
+    public static ILocalizerBuilder<T> Create(IStringLocalizer<T> stringLocalizer) => new LocalizerBuilder<T>(stringLocalizer);
 
     public IStringLocalizer<T> Build() => _localizer;
 
-    public ILocalizerIndexerSubstituteBuilder<T> For(string name, params object[] args)
+    public ILocalizerIndexerSubstituteBuilder<T> For(string name, params object[]? args)
     {
         _name = name;
         _args = args;
@@ -30,7 +34,7 @@ public class LocalizerBuilder<T> : ILocalizerIndexerSubstituteBuilder<T>, ILocal
     public ILocalizerBuilder<T> Return(string value)
     {
         ArgumentNullException.ThrowIfNull(_name, nameof(_name));
-        ArgumentNullException.ThrowIfNull(_args, nameof(_args));
+        //ArgumentNullException.ThrowIfNull(_args, nameof(_args));
 
         _localizer.Configure()
             [_name, _args]
