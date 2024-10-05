d935544b92e08cdfe52653cc021492089793a30e
Daniil P <daniilpoyarkov@gmail.com>
2024-10-05 20:38:57 +0300

    Added job

--- src/Eclipse.Pipelines/Jobs/SendMoodReport/SendMoodReportJob.cs
+++ src/Eclipse.Pipelines/Jobs/SendMoodReport/SendMoodReportJob.cs
@@ -1,19 +1,27 @@
 ﻿using Eclipse.Common.Clock;
+using Eclipse.Common.Plots;
 using Eclipse.Domain.MoodRecords;
+using Eclipse.Domain.Shared.MoodRecords;
 using Eclipse.Domain.Users;
 using Eclipse.Localization.Culture;
+using User = Eclipse.Domain.Users.User;
 
 using Microsoft.Extensions.Localization;
 
 using Quartz;
 
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using Eclipse.Localization.Extensions;
 
 namespace Eclipse.Pipelines.Jobs.SendMoodReport;
 
 internal sealed class SendMoodReportJob : EclipseJobBase
 {
     private static readonly TimeOnly _time = new(19, 0);
+
+    private static readonly TimeOnly _recordTime = new(0, 0);
+
 
     private readonly IUserRepository _userRepository;
 
@@ -27,13 +35,16 @@ internal sealed class SendMoodReportJob : EclipseJobBase
 
     private readonly ICurrentCulture _currentCulture;
 
+    private readonly IPlotGenerator _plotGenerator;
+
     public SendMoodReportJob(
         IUserRepository userRepository,
         IMoodRecordRepository moodRecordRepository,
         ITelegramBotClient client,
         IStringLocalizer<SendMoodReportJob> localizer,
         ITimeProvider timeProvider,
-        ICurrentCulture currentCulture)
+        ICurrentCulture currentCulture,
+        IPlotGenerator plotGenerator)
     {
         _userRepository = userRepository;
         _moodRecordRepository = moodRecordRepository;
@@ -41,13 +52,14 @@ internal sealed class SendMoodReportJob : EclipseJobBase
         _localizer = localizer;
         _timeProvider = timeProvider;
         _currentCulture = currentCulture;
+        _plotGenerator = plotGenerator;
     }
 
     public override async Task Execute(IJobExecutionContext context)
     {
         var date = _timeProvider.Now;
         var time = date.GetTime();
-        
+
         var users = (await _userRepository.GetByExpressionAsync(u => u.NotificationsEnabled, context.CancellationToken))
             .Where(u => time.Add(u.Gmt) == _time)
             .ToList();
@@ -59,11 +71,11 @@ internal sealed class SendMoodReportJob : EclipseJobBase
             context.CancellationToken
         );
 
-        var grouping = moodRecords.GroupBy(mr => mr.UserId);
+        var groups = moodRecords.GroupBy(mr => mr.UserId);
 
         var sendings = new List<Task>(users.Count);
 
-        foreach (var group in grouping)
+        foreach (var group in groups)
         {
             if (group.IsNullOrEmpty())
             {
@@ -77,17 +89,61 @@ internal sealed class SendMoodReportJob : EclipseJobBase
                 continue;
             }
 
-            using var _ = _currentCulture.UsingCulture(user.Culture);
+            var records = group
+                .OrderBy(r => r.CreatedAt)
+                .ToList();
 
-            var message = group
-                .Select(mr => $"{mr.CreatedAt:dd.MM HH.mm} you felt {mr.State}")
-                .Join(Environment.NewLine);
+            var days = new DateTime[records.Count];
+            var states = new int[records.Count];
+
+            for (int i = 0; i < records.Count; i++)
+            {
+                var record = records[i];
+
+                days[i] = record.CreatedAt.WithTime(_recordTime);
+                states[i] = GetMoodScore(record.State);
+            }
+
+            var options = new PlotOptions<DateTime, int>
+            {
+                Title = $"{days[0]:dd.MM}-{days[^1]:dd.MM}",
+                YAxisTitle = "Score",
+                Width = 550,
+                Height = 300,
+                Ys = states,
+                Xs = days
+            };
 
             sendings.Add(
-                _client.SendTextMessageAsync(user.ChatId, message, cancellationToken: context.CancellationToken)
+                SendReportAsync(context, user, options)
             );
         }
 
         await Task.WhenAll(sendings);
+    }
+
+    private async Task SendReportAsync(IJobExecutionContext context, User user, PlotOptions<DateTime, int> options)
+    {
+        using var _ = _currentCulture.UsingCulture(user.Culture);
+        _localizer.UseCurrentCulture(_currentCulture);
+
+        var message = _localizer["Jobs:SendMoodReport:Caption"].Value;
+
+        using var stream = _plotGenerator.Create(options);
+
+        await _client.SendPhotoAsync(user.ChatId,
+            InputFile.FromStream(stream, $"mood-report.png"),
+            caption: message,
+            cancellationToken: context.CancellationToken
+        );
+    }
+
+    private static int GetMoodScore(MoodState state)
+    {
+        return state switch
+        {
+            MoodState.Good => 1,
+            _ => 0,
+        };
     }
 }
