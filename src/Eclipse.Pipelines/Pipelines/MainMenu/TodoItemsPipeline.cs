fc8feaf5ab58827a44bd0ba75ed21f34ef61d8eb
Daniil P <daniilpoyarkov@gmail.com>
2024-10-06 21:10:37 +0300

    Naming

--- src/Eclipse.Pipelines/Pipelines/MainMenu/TodoItemsPipeline.cs
+++ src/Eclipse.Pipelines/Pipelines/MainMenu/TodoItemsPipeline.cs
@@ -4,7 +4,7 @@ using Eclipse.Pipelines.Pipelines.MainMenu.TodoItems;
 namespace Eclipse.Pipelines.Pipelines.MainMenu;
 
 [Route("Menu:MainMenu:MyToDos", "/todos")]
-internal sealed class TodoItemsMenuPipeline : TodoItemsPipelineBase
+internal sealed class TodoItemsPipeline : TodoItemsPipelineBase
 {
     protected override void Initialize()
     {
