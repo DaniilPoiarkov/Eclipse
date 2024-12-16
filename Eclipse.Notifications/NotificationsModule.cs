61241461d05e8edc83ddd1c2da73a963823a58e4
Daniil P <daniilpoyarkov@gmail.com>
2024-12-17 00:40:59 +0200

    Test setup

---
+++ Eclipse.Notifications/NotificationsModule.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eclipse.Notifications;
+
+public static class NotificationsModule
+{
+    public static IServiceCollection AddNotificationsModule(this IServiceCollection services)
+    {
+        services.AddMassTransit(bus =>
+        {
+            bus.UsingInMemory((context, configurator) =>
+            {
+                configurator.ConfigureEndpoints(context);
+            });
+
+            bus.AddConsumers(typeof(NotificationsModule).Assembly);
+        });
+
+        return services;
+    }
+}
