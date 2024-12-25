fa2c3ff36a050239107742f51748b9fc0f69dc52
Daniil P <daniilpoyarkov@gmail.com>
2024-12-25 17:53:19 +0200

    Added repos

---
+++ src/Eclipse.DataAccess/InboxMessages/CachedInboxMessageRepository.cs
@@ -0,0 +1,26 @@
+﻿using Eclipse.Common.Caching;
+using Eclipse.DataAccess.Repositories;
+using Eclipse.Domain.InboxMessages;
+
+namespace Eclipse.DataAccess.InboxMessages;
+
+internal sealed class CachedInboxMessageRepository : CachedRepositoryBase<InboxMessage, IInboxMessageRepository>, IInboxMessageRepository
+{
+    public CachedInboxMessageRepository(IInboxMessageRepository repository, ICacheService cacheService)
+        : base(repository, cacheService) { }
+
+    public Task DeleteSuccessfullyProcessedAsync(CancellationToken cancellationToken = default)
+    {
+        return Repository.DeleteSuccessfullyProcessedAsync(cancellationToken);
+    }
+
+    public Task<List<InboxMessage>> GetPendingAsync(int count, CancellationToken cancellationToken = default)
+    {
+        return CacheService.GetOrCreateAsync(
+            $"{GetPrefix()}-pending-{count}",
+            () => Repository.GetPendingAsync(count, cancellationToken),
+            CacheConsts.FiveMinutes,
+            cancellationToken
+        );
+    }
+}
---
+++ src/Eclipse.DataAccess/InboxMessages/InboxMessageRepository.cs
@@ -0,0 +1,28 @@
+﻿using Eclipse.DataAccess.CosmosDb;
+using Eclipse.DataAccess.Repositories;
+using Eclipse.Domain.InboxMessages;
+using Eclipse.Domain.Shared.InboxMessages;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Eclipse.DataAccess.InboxMessages;
+
+internal sealed class InboxMessageRepository : RepositoryBase<InboxMessage>, IInboxMessageRepository
+{
+    public InboxMessageRepository(EclipseDbContext context)
+        : base(context) { }
+
+    public Task DeleteSuccessfullyProcessedAsync(CancellationToken cancellationToken = default)
+    {
+        return DbSet.Where(i => i.Status == InboxMessageStatus.Processed)
+            .ExecuteDeleteAsync(cancellationToken);
+    }
+
+    public Task<List<InboxMessage>> GetPendingAsync(int count, CancellationToken cancellationToken = default)
+    {
+        return DbSet.Where(i => i.Status == InboxMessageStatus.Pending)
+            .OrderBy(i => i.OccuredAt)
+            .Take(count)
+            .ToListAsync(cancellationToken);
+    }
+}
