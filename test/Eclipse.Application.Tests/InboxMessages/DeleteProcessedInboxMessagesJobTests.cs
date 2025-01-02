287b680acb5ac697adf85408c36ac099740683a6
Daniil P <daniilpoyarkov@gmail.com>
2025-01-02 17:28:08 +0200

    Added test

---
+++ test/Eclipse.Application.Tests/InboxMessages/DeleteProcessedInboxMessagesJobTests.cs
@@ -0,0 +1,33 @@
+﻿using Eclipse.Application.Contracts.InboxMessages;
+using Eclipse.Application.InboxMessages;
+
+using NSubstitute;
+
+using Quartz;
+
+using Xunit;
+
+namespace Eclipse.Application.Tests.InboxMessages;
+
+public sealed class DeleteProcessedInboxMessagesJobTests
+{
+    private readonly IInboxMessageService _inboxMessageService;
+
+    private readonly DeleteProcessedInboxMessagesJob _sut;
+
+    public DeleteProcessedInboxMessagesJobTests()
+    {
+        _inboxMessageService = Substitute.For<IInboxMessageService>();
+        _sut = new DeleteProcessedInboxMessagesJob(_inboxMessageService);
+    }
+
+    [Fact]
+    public async Task Execute_WhenCalled_ThenDelegatesCallToService()
+    {
+        var context = Substitute.For<IJobExecutionContext>();
+
+        await _sut.Execute(context);
+
+        await _inboxMessageService.Received().DeleteProcessedAsync(context.CancellationToken);
+    }
+}
---
+++ test/Eclipse.Application.Tests/InboxMessages/InboxMessageServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Eclipse.Application.InboxMessages;
+using Eclipse.Domain.InboxMessages;
+
+using NSubstitute;
+
+using Xunit;
+
+namespace Eclipse.Application.Tests.InboxMessages;
+
+public sealed class InboxMessageServiceTests
+{
+    private readonly IInboxMessageRepository _repository;
+
+    private readonly InboxMessageService _sut;
+
+    public InboxMessageServiceTests()
+    {
+        _repository = Substitute.For<IInboxMessageRepository>();
+        _sut = new InboxMessageService(_repository);
+    }
+
+    [Fact]
+    public async Task DeleteProcessedAsync_WhenCalled_ThenDelegatesCallToRepository()
+    {
+        var cancellationToken = new CancellationToken();
+        await _sut.DeleteProcessedAsync(cancellationToken);
+        await _repository.Received().DeleteSuccessfullyProcessedAsync(cancellationToken);
+    }
+}
