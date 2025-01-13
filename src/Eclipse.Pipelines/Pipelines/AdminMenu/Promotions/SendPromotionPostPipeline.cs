2c8adaf213ab3cf83917546a1cf1271c40f71df5
Daniil P <daniilpoyarkov@gmail.com>
2025-01-13 22:26:27 +0200

    Added confirmation step and localized messages + minor fix

--- src/Eclipse.Pipelines/Pipelines/AdminMenu/Promotions/SendPromotionPostPipeline.cs
+++ src/Eclipse.Pipelines/Pipelines/AdminMenu/Promotions/SendPromotionPostPipeline.cs
@@ -1,6 +1,10 @@
 ﻿using Eclipse.Common.Caching;
 using Eclipse.Core.Attributes;
 using Eclipse.Core.Core;
+using Eclipse.Localization.Localizers;
+
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Eclipse.Pipelines.Pipelines.AdminMenu.Promotions;
 
@@ -9,14 +13,18 @@ internal sealed class SendPromotionPostPipeline : AdminPipelineBase
 {
     private readonly ICacheService _cacheService;
 
-    public SendPromotionPostPipeline(ICacheService cacheService)
+    private readonly ITelegramBotClient _botClient;
+
+    public SendPromotionPostPipeline(ICacheService cacheService, ITelegramBotClient botClient)
     {
         _cacheService = cacheService;
+        _botClient = botClient;
     }
 
     protected override void Initialize()
     {
         RegisterStage(RequestPostMessage);
+        RegisterStage(ReviewPostMessageAsync);
         RegisterStage(SendConfirmationCodeAsync);
         RegisterStage(SendPromotionPostAsync);
     }
@@ -26,8 +34,28 @@ internal sealed class SendPromotionPostPipeline : AdminPipelineBase
         return Text(Localizer["Pipelines:Admin:Promotions:Post:Request"]);
     }
 
+    private async Task<IResult> ReviewPostMessageAsync(MessageContext context, CancellationToken cancellationToken)
+    {
+        // TODO: Replace with actual message id
+        await _botClient.CopyMessage(context.ChatId, context.ChatId, 15746, cancellationToken: cancellationToken);
+
+        return Menu(
+            [ 
+                [ new KeyboardButton(Localizer["Pipelines:Admin:Promotions:Post:Review:Continue"]) ],
+                [ new KeyboardButton(Localizer["Pipelines:Admin:Promotions:Post:Review:Cancel"]) ]
+            ],
+            Localizer["Pipelines:Admin:Promotions:Post:Review"]
+        );
+    }
+
     private async Task<IResult> SendConfirmationCodeAsync(MessageContext context, CancellationToken cancellationToken)
     {
+        if (!ContinuePromotionProcessing(context))
+        {
+            FinishPipeline();
+            return Menu(AdminMenuButtons, Localizer["Pipelines:Admin:Promotions:Post:Cancelled"]);
+        }
+
         var confirmationCode = Enumerable.Range(0, 6)
             .Select(_ => Random.Shared.Next(0, 10))
             .Aggregate(string.Empty, (s, i) => $"{s}{i}");
@@ -48,9 +76,15 @@ internal sealed class SendPromotionPostPipeline : AdminPipelineBase
 
         if (context.Value.Equals(confirmationCode))
         {
-            return Text(Localizer["Pipelines:Admin:Promotions:Post:Confirmed"]);
+            return Menu(AdminMenuButtons, Localizer["Pipelines:Admin:Promotions:Post:Confirmed"]);
         }
 
-        return Text(Localizer["Pipelines:Admin:Promotions:Post:ConfirmationFailed"]);
+        return Menu(AdminMenuButtons, Localizer["Pipelines:Admin:Promotions:Post:ConfirmationFailed"]);
+    }
+
+    private bool ContinuePromotionProcessing(MessageContext context)
+    {
+        return Localizer.TryConvertToLocalizableString(context.Value, out var localized)
+            && localized.Equals("Pipelines:Admin:Promotions:Post:Review:Continue");
     }
 }
