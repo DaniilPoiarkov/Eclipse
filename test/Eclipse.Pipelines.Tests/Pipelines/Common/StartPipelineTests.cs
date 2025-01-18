9bb8568850a975dc66441a1464191a2f9ab4d520
Daniil P <daniilpoyarkov@gmail.com>
2025-01-18 22:55:59 +0200

    Fixed test

--- test/Eclipse.Pipelines.Tests/Pipelines/Common/StartPipelineTests.cs
+++ test/Eclipse.Pipelines.Tests/Pipelines/Common/StartPipelineTests.cs
@@ -1,10 +1,19 @@
-﻿using Eclipse.Core.Results;
+﻿using Eclipse.Application.Contracts.Configuration;
+using Eclipse.Application.Contracts.Users;
+using Eclipse.Common.Results;
+using Eclipse.Core.Results;
+using Eclipse.Localization.Culture;
+using Eclipse.Pipelines.Culture;
 using Eclipse.Pipelines.Pipelines.Common;
+using Eclipse.Pipelines.Stores.Messages;
 using Eclipse.Pipelines.Tests.Fixture;
+using Eclipse.Tests.Generators;
 
 using FluentAssertions;
 
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
 
 using NSubstitute;
 
@@ -12,17 +21,37 @@ using Xunit;
 
 namespace Eclipse.Pipelines.Tests.Pipelines.Common;
 
-public class StartPipelineTests : PipelineTestFixture<StartPipeline>
+public sealed class StartPipelineTests : PipelineTestFixture<StartPipeline>
 {
-    public StartPipelineTests()
+    protected override void ConfigureServices(IServiceCollection services)
     {
-        Localizer[""].ReturnsForAnyArgs(new LocalizedString("{name}", "{name}"));
+        services.AddSingleton(Substitute.For<ICultureTracker>())
+            .AddSingleton(Substitute.For<IUserService>())
+            .AddSingleton(Substitute.For<IMessageStore>())
+            .AddSingleton(Substitute.For<IOptions<CultureList>>())
+            .AddSingleton(Substitute.For<ICurrentCulture>());
     }
 
     [Fact]
     public async Task RunNext_WhenExecuted_ThenTextReturned()
     {
+        var userService = ServiceProvider.GetRequiredService<IUserService>();
+        
+        var user = UserGenerator.Get();
         var context = GetContext("/start");
+
+        userService.GetByChatIdAsync(context.ChatId)
+            .Returns(new UserDto
+            {
+                Id = user.Id,
+                ChatId = context.ChatId,
+                Name = user.Name,
+                Surname = user.Surname,
+                UserName = user.UserName,
+                Culture = user.Culture,
+            });
+
+        Localizer["", Arg.Any<object[]>()].ReturnsForAnyArgs(new LocalizedString("{0}", context.User.Name));
 
         var result = await Sut.RunNext(context);
 
