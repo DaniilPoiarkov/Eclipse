0d323f4e96eb6bbc90069c9fd315937843684562
Daniil P <daniilpoyarkov@gmail.com>
2025-02-01 23:30:58 +0200

    Removed irrelevant logging statement

--- src/Eclipse.Pipelines/EclipsePipelinesModule.cs
+++ src/Eclipse.Pipelines/EclipsePipelinesModule.cs
@@ -106,12 +106,10 @@ public static class EclipsePipelinesModule
     {
         var options = serviceProvider.GetRequiredService<IOptions<PipelinesOptions>>();
         var appUrlProvider = serviceProvider.GetRequiredService<IAppUrlProvider>();
-        var logger = serviceProvider.GetRequiredService<ILogger<IAppUrlProvider>>();
 
         var webhookInfo = await client.GetWebhookInfo();
 
         var webhook = $"{appUrlProvider.AppUrl.EnsureEndsWith('/')}{options.Value.ActiveEndpoint}";
-        logger.LogInformation("Attempt to set webhook {Webhook}", webhook);
 
         if (webhookInfo is not null && webhookInfo.Url.Equals(webhook))
         {
