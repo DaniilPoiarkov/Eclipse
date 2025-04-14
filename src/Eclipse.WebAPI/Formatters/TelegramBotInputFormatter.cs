8748267d63e4f18cc08b7d4ad74bbf8ce54ff96b
Daniil P <daniilpoyarkov@gmail.com>
2025-04-14 22:55:56 +0300

    Dropped deprecated package. SpecFlow retired

---
+++ src/Eclipse.WebAPI/Formatters/TelegramBotInputFormatter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+
+using System.Text;
+using System.Text.Json;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Eclipse.WebAPI.Formatters;
+
+internal sealed class TelegramBotInputFormatter : TextInputFormatter
+{
+    public TelegramBotInputFormatter()
+    {
+        SupportedEncodings.Add(Encoding.UTF8);
+        SupportedMediaTypes.Add("application/json");
+    }
+
+    protected override bool CanReadType(Type type) => type == typeof(Update);
+
+    public sealed override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+    {
+        var model = await JsonSerializer.DeserializeAsync(
+            context.HttpContext.Request.Body,
+            context.ModelType,
+            JsonBotAPI.Options,
+            context.HttpContext.RequestAborted
+        );
+
+        return await InputFormatterResult.SuccessAsync(model);
+    }
+}
---
+++ src/Eclipse.WebAPI/Formatters/TelegramBotOutputFormatter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+
+using System.Text;
+using System.Text.Json;
+
+using Telegram.Bot.Requests.Abstractions;
+
+using Telegram.Bot;
+
+namespace Eclipse.WebAPI.Formatters;
+
+internal sealed class TelegramBotOutputFormatter : TextOutputFormatter
+{
+    public TelegramBotOutputFormatter()
+    {
+        SupportedEncodings.Add(Encoding.UTF8);
+        SupportedMediaTypes.Add("application/json");
+    }
+
+    protected override bool CanWriteType(Type? type) => typeof(IRequest).IsAssignableFrom(type);
+
+    public sealed override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+    {
+        var stream = context.HttpContext.Response.Body;
+        await JsonSerializer.SerializeAsync(stream, context.Object, JsonBotAPI.Options, context.HttpContext.RequestAborted);
+    }
+}
