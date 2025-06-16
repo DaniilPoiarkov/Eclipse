85336299a4ed87a2498ee062f378c50b63902107
Daniil P <daniilpoyarkov@gmail.com>
2025-06-17 00:43:43 +0300

    fmt

--- infrastructure/modules/monitoring/main.tf
+++ infrastructure/modules/monitoring/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
   location            = var.location
   resource_group_name = var.resource_group_name
   name                = "${var.app_name}-${var.environment}-log"
-  tags = local.tags
+  tags                = local.tags
 }
 
 resource "azurerm_application_insights" "app_insights" {
@@ -18,7 +18,7 @@ resource "azurerm_application_insights" "app_insights" {
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = var.application_type
-  tags = local.tags
+  tags                = local.tags
 
   depends_on = [
     azurerm_log_analytics_workspace.log_analytics_workspace
