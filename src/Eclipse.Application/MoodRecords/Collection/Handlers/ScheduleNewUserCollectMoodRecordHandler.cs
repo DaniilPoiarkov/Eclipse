78c1dd62476e047b04ae85d91d3a949fcf162f4b
Daniil P <daniilpoyarkov@gmail.com>
2025-09-07 16:37:21 +0300

    Refactored mood report collection jobs

--- src/Eclipse.Application/MoodRecords/Collection/Handlers/ScheduleNewUserCollectMoodRecordHandler.cs
+++ src/Eclipse.Application/MoodRecords/Collection/Handlers/ScheduleNewUserCollectMoodRecordHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Eclipse.Common.Notifications;
 using Eclipse.Domain.Users;
 using Eclipse.Domain.Users.Events;
+using Eclipse.Domain.Users.Handlers;
 
 using Microsoft.Extensions.Logging;
 
@@ -8,19 +9,22 @@ using Quartz;
 
 namespace Eclipse.Application.MoodRecords.Collection.Handlers;
 
-internal sealed class ScheduleNewUserCollectMoodRecordHandler : CollectMoodRecordHandlerBase<NewUserJoinedDomainEvent>
+internal sealed class ScheduleNewUserCollectMoodRecordHandler : UserEventHandlerBase<NewUserJoinedDomainEvent, CollectMoodRecordJob, CollectMoodRecordSchedulerOptions>
 {
     public ScheduleNewUserCollectMoodRecordHandler(
         IUserRepository userRepository,
         ISchedulerFactory schedulerFactory,
         INotificationScheduler<CollectMoodRecordJob, CollectMoodRecordSchedulerOptions> jobScheduler,
-        ILogger<ScheduleNewUserCollectMoodRecordHandler> logger) : base(userRepository, schedulerFactory, jobScheduler, logger)
-    {
-
-    }
+        ILogger<ScheduleNewUserCollectMoodRecordHandler> logger)
+        : base(userRepository, schedulerFactory, jobScheduler, logger) { }
 
     public override Task Handle(NewUserJoinedDomainEvent @event, CancellationToken cancellationToken = default)
     {
         return Handle(@event.UserId, cancellationToken);
+    }
+
+    protected override CollectMoodRecordSchedulerOptions GetOptions(User user)
+    {
+        return new CollectMoodRecordSchedulerOptions(user.Id, user.Gmt);
     }
 }
--- src/Eclipse.Application/MoodRecords/Collection/Handlers/ScheduleUserEnabledCollectMoodRecordHandler.cs
+++ src/Eclipse.Application/MoodRecords/Collection/Handlers/ScheduleUserEnabledCollectMoodRecordHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Eclipse.Common.Notifications;
 using Eclipse.Domain.Users;
 using Eclipse.Domain.Users.Events;
+using Eclipse.Domain.Users.Handlers;
 
 using Microsoft.Extensions.Logging;
 
@@ -8,18 +9,22 @@ using Quartz;
 
 namespace Eclipse.Application.MoodRecords.Collection.Handlers;
 
-internal sealed class ScheduleUserEnabledCollectMoodRecordHandler : CollectMoodRecordHandlerBase<UserEnabledDomainEvent>
+internal sealed class ScheduleUserEnabledCollectMoodRecordHandler : UserEventHandlerBase<UserEnabledDomainEvent, CollectMoodRecordJob, CollectMoodRecordSchedulerOptions>
 {
     public ScheduleUserEnabledCollectMoodRecordHandler(
         IUserRepository userRepository,
         ISchedulerFactory schedulerFactory,
         INotificationScheduler<CollectMoodRecordJob, CollectMoodRecordSchedulerOptions> jobScheduler,
-        ILogger<ScheduleUserEnabledCollectMoodRecordHandler> logger) : base(userRepository, schedulerFactory, jobScheduler, logger)
-    {
-    }
+        ILogger<ScheduleUserEnabledCollectMoodRecordHandler> logger)
+        : base(userRepository, schedulerFactory, jobScheduler, logger) { }
 
     public override Task Handle(UserEnabledDomainEvent @event, CancellationToken cancellationToken = default)
     {
         return Handle(@event.UserId, cancellationToken);
+    }
+
+    protected override CollectMoodRecordSchedulerOptions GetOptions(User user)
+    {
+        return new CollectMoodRecordSchedulerOptions(user.Id, user.Gmt);
     }
 }
