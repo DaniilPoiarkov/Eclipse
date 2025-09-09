e37161334edb65268315c494c12986b1c8608026
Daniil P <daniilpoyarkov@gmail.com>
2025-09-09 20:34:33 +0300

    Added base rescheduler

---
+++ src/Eclipse.Application/Jobs/JobRescheduler.cs
@@ -0,0 +1,38 @@
+﻿using Eclipse.Common.Background;
+using Eclipse.Common.Notifications;
+using Eclipse.Domain.Users;
+
+using Quartz;
+
+namespace Eclipse.Application.Jobs;
+
+internal abstract class JobRescheduler<TJob, TSchedulerOptions> : IBackgroundJob
+    where TJob : IJob
+    where TSchedulerOptions : ISchedulerOptions
+{
+    private readonly IUserRepository _userRepository;
+
+    private readonly ISchedulerFactory _schedulerFactory;
+
+    private readonly INotificationScheduler<TJob, TSchedulerOptions> _jobScheduler;
+
+    public JobRescheduler(IUserRepository userRepository, ISchedulerFactory schedulerFactory, INotificationScheduler<TJob, TSchedulerOptions> jobScheduler)
+    {
+        _userRepository = userRepository;
+        _schedulerFactory = schedulerFactory;
+        _jobScheduler = jobScheduler;
+    }
+
+    public async Task Execute(CancellationToken cancellationToken = default)
+    {
+        var users = await _userRepository.GetByExpressionAsync(u => u.IsEnabled, cancellationToken);
+        var scheduelr = await _schedulerFactory.GetScheduler(cancellationToken);
+
+        foreach (var user in users)
+        {
+            await _jobScheduler.Schedule(scheduelr, GetOptions(user), cancellationToken);
+        }
+    }
+
+    protected abstract TSchedulerOptions GetOptions(User user);
+}
