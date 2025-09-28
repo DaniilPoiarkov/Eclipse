fe8962832ef68814e0965fc536e534d82bd53674
Daniil P <daniilpoyarkov@gmail.com>
2025-09-28 21:23:45 +0300

    Unified feedback options

--- src/Eclipse.Application/Feedbacks/Collection/Handlers/ScheduleNewUserCollectFeedbackHandler.cs
+++ src/Eclipse.Application/Feedbacks/Collection/Handlers/ScheduleNewUserCollectFeedbackHandler.cs
@@ -1,4 +1,5 @@
-﻿using Eclipse.Common.Notifications;
+﻿using Eclipse.Application.Jobs;
+using Eclipse.Common.Notifications;
 using Eclipse.Domain.Users;
 using Eclipse.Domain.Users.Events;
 using Eclipse.Domain.Users.Handlers;
@@ -9,12 +10,12 @@ using Quartz;
 
 namespace Eclipse.Application.Feedbacks.Collection.Handlers;
 
-internal sealed class ScheduleNewUserCollectFeedbackHandler : UserEventHandlerBase<NewUserJoinedDomainEvent, CollectFeedbackJob, CollectFeedbackSchedulerOptions>
+internal sealed class ScheduleNewUserCollectFeedbackHandler : UserEventHandlerBase<NewUserJoinedDomainEvent, CollectFeedbackJob, SchedulerOptions>
 {
     public ScheduleNewUserCollectFeedbackHandler(
         IUserRepository userRepository,
         ISchedulerFactory schedulerFactory,
-        INotificationScheduler<CollectFeedbackJob, CollectFeedbackSchedulerOptions> jobScheduler,
+        INotificationScheduler<CollectFeedbackJob, SchedulerOptions> jobScheduler,
         ILogger<ScheduleNewUserCollectFeedbackHandler> logger)
         : base(userRepository, schedulerFactory, jobScheduler, logger) { }
 
@@ -23,8 +24,8 @@ internal sealed class ScheduleNewUserCollectFeedbackHandler : UserEventHandlerBa
         return Handle(@event.UserId, cancellationToken);
     }
 
-    protected override CollectFeedbackSchedulerOptions GetOptions(User user)
+    protected override SchedulerOptions GetOptions(User user)
     {
-        return new CollectFeedbackSchedulerOptions(user.Id, user.Gmt);
+        return new SchedulerOptions(user.Id, user.Gmt);
     }
 }
