37a315566a8a8ff5dd62393a1948bfe1a9feab5e
Daniil P <daniilpoyarkov@gmail.com>
2025-09-28 22:54:18 +0300

    Refactored job for new time rescheduling

--- src/Eclipse.Application/Feedbacks/Collection/Handlers/ScheduleNewTimeCollectFeedbackHandler.cs
+++ src/Eclipse.Application/Feedbacks/Collection/Handlers/ScheduleNewTimeCollectFeedbackHandler.cs
@@ -1,32 +1,32 @@
-﻿using Eclipse.Application.Jobs;
-using Eclipse.Common.Events;
-using Eclipse.Common.Notifications;
-using Eclipse.Domain.Users.Events;
+﻿//using Eclipse.Application.Jobs;
+//using Eclipse.Common.Events;
+//using Eclipse.Common.Notifications;
+//using Eclipse.Domain.Users.Events;
 
-using Quartz;
+//using Quartz;
 
-namespace Eclipse.Application.Feedbacks.Collection.Handlers;
+//namespace Eclipse.Application.Feedbacks.Collection.Handlers;
 
-internal sealed class ScheduleNewTimeCollectFeedbackHandler : IEventHandler<GmtChangedDomainEvent>
-{
-    private readonly ISchedulerFactory _schedulerFactory;
+//internal sealed class ScheduleNewTimeCollectFeedbackHandler : IEventHandler<GmtChangedDomainEvent>
+//{
+//    private readonly ISchedulerFactory _schedulerFactory;
 
-    private readonly INotificationScheduler<CollectFeedbackJob, SchedulerOptions> _jobScheduler;
+//    private readonly INotificationScheduler<CollectFeedbackJob, SchedulerOptions> _jobScheduler;
 
-    public ScheduleNewTimeCollectFeedbackHandler(
-        ISchedulerFactory schedulerFactory,
-        INotificationScheduler<CollectFeedbackJob, SchedulerOptions> jobScheduler)
-    {
-        _schedulerFactory = schedulerFactory;
-        _jobScheduler = jobScheduler;
-    }
+//    public ScheduleNewTimeCollectFeedbackHandler(
+//        ISchedulerFactory schedulerFactory,
+//        INotificationScheduler<CollectFeedbackJob, SchedulerOptions> jobScheduler)
+//    {
+//        _schedulerFactory = schedulerFactory;
+//        _jobScheduler = jobScheduler;
+//    }
 
-    public async Task Handle(GmtChangedDomainEvent @event, CancellationToken cancellationToken = default)
-    {
-        var scheduler = await _schedulerFactory.GetScheduler(cancellationToken);
-        var options = new SchedulerOptions(@event.UserId, @event.Gmt);
+//    public async Task Handle(GmtChangedDomainEvent @event, CancellationToken cancellationToken = default)
+//    {
+//        var scheduler = await _schedulerFactory.GetScheduler(cancellationToken);
+//        var options = new SchedulerOptions(@event.UserId, @event.Gmt);
 
-        await _jobScheduler.Unschedule(scheduler, options, cancellationToken);
-        await _jobScheduler.Schedule(scheduler, options, cancellationToken);
-    }
-}
+//        await _jobScheduler.Unschedule(scheduler, options, cancellationToken);
+//        await _jobScheduler.Schedule(scheduler, options, cancellationToken);
+//    }
+//}
