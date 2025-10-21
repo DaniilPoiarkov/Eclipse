c422a591c6e65c1ceed0c1f1c67c03381bd0a137
unknown <daniilpoyarkov@gmail.com>
2025-10-21 21:17:09 +0300

    Added support for purchased paid media update types

---
+++ src/Common/Eclipse.Core/Provider/Handlers/PurchasedPaidMediaHandler.cs
@@ -0,0 +1,28 @@
+﻿using Eclipse.Core.Pipelines;
+using Eclipse.Core.Routing;
+
+using System.Reflection;
+
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Eclipse.Core.Provider.Handlers;
+
+internal sealed class PurchasedPaidMediaHandler : IRouteHandler
+{
+    private readonly IEnumerable<PipelineBase> _pipelines;
+
+    public PurchasedPaidMediaHandler(IEnumerable<PipelineBase> pipelines)
+    {
+        _pipelines = pipelines;
+    }
+
+    public UpdateType Type => UpdateType.PurchasedPaidMedia;
+
+    public PipelineBase? Get(Update update)
+    {
+        return _pipelines.FirstOrDefault(p => p.GetType()
+            .GetCustomAttribute<PurchasedPaidMediaPipelineAttribute>() is not null
+        );
+    }
+}
---
+++ src/Common/Eclipse.Core/Routing/PurchasedPaidMediaPipelineAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Eclipse.Core.Routing;
+
+/// <summary>
+/// Pipeline for processing PurchasedPaidMedia updates.
+/// Keep in mind that only one pipeline with this attribute should be registered.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class PurchasedPaidMediaPipelineAttribute : Attribute
+{
+
+}
