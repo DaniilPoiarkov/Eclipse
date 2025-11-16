5521a3fe2e08023a147cbc42862dfea9b9cc743b
unknown <daniilpoyarkov@gmail.com>
2025-11-16 18:12:36 +0200

    Added config for plots for montly reports

---
+++ src/Eclipse.Infrastructure/Plots/PlotTemplateFactory.cs
@@ -0,0 +1,15 @@
+﻿using Eclipse.Infrastructure.Plots.Templates;
+
+namespace Eclipse.Infrastructure.Plots;
+
+internal sealed class PlotTemplateFactory : IPlotTemplateFactory
+{
+    public IPlotTemplate Create(PlotTemplateType type)
+    {
+        return type switch
+        {
+            PlotTemplateType.SegoeDark => new SegoeDarkTemplate(),
+            _ => throw new NotSupportedException($"Plot template type '{type}' is not supported.")
+        };
+    }
+}
---
+++ src/Eclipse.Infrastructure/Plots/Templates/SegoeDarkTemplate.cs
@@ -0,0 +1,33 @@
+﻿using ScottPlot;
+
+namespace Eclipse.Infrastructure.Plots.Templates;
+
+internal sealed class SegoeDarkTemplate : IPlotTemplate
+{
+    private static readonly Color _dataBackgroundColor = Color.FromHex("#0b3049");
+
+    private static readonly Color _figureColor = Color.FromHex("#07263b");
+
+    private static readonly Color _axesColor = Color.FromHex("#a0acb5");
+
+    private static readonly string _font = "Segoe UI";
+
+    private static readonly float _minorTickSize = 0.2f;
+
+    public Plot Create()
+    {
+        var plot = new Plot();
+
+        plot.DataBackground.Color = _dataBackgroundColor;
+        plot.FigureBackground.Color = _figureColor;
+
+        plot.Axes.Color(_axesColor);
+
+        plot.Axes.Bottom.MinorTickStyle.Length = _minorTickSize;
+        plot.Axes.Left.MinorTickStyle.Length = _minorTickSize;
+
+        plot.Font.Set(_font);
+
+        return plot;
+    }
+}
