50012b2e0de14594b262b060f4e1daf4bd0ef2e2
unknown <daniilpoyarkov@gmail.com>
2025-11-23 21:52:57 +0200

    Initial setup for dynamic themes

--- src/Eclipse.Infrastructure/Plots/Templates/SansLightTemplate.cs
+++ src/Eclipse.Infrastructure/Plots/Templates/SansLightTemplate.cs
@@ -2,13 +2,13 @@
 
 namespace Eclipse.Infrastructure.Plots.Templates;
 
-internal sealed class SansDarkTemplate : IPlotTemplate
+internal sealed class SansLightTemplate : IPlotTemplate
 {
-    private static readonly Color _dataBackgroundColor = Color.FromHex("#0b3049");
+    private static readonly Color _dataBackgroundColor = Color.FromHex("#CBF3BB");
 
-    private static readonly Color _figureColor = Color.FromHex("#07263b");
+    private static readonly Color _figureColor = Color.FromHex("#ABE7B2");
 
-    private static readonly Color _axesColor = Color.FromHex("#a0acb5");
+    private static readonly Color _axesColor = Color.FromHex("#93BFC7");
 
     public Plot Create()
     {
@@ -20,7 +20,10 @@ internal sealed class SansDarkTemplate : IPlotTemplate
         plot.Axes.Color(_axesColor);
 
         plot.Axes.Bottom.MinorTickStyle.Length = TemplateConsts.MinorTickSize;
+        plot.Axes.Bottom.TickLabelStyle.ForeColor = _axesColor;
+
         plot.Axes.Left.MinorTickStyle.Length = TemplateConsts.MinorTickSize;
+        plot.Axes.Left.TickLabelStyle.ForeColor = _axesColor;
 
         plot.Font.Set(TemplateConsts.SansFont);
 
