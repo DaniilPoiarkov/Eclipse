ce1ae7fc1e39c992300f13732a76193893bea57b
unknown <daniilpoyarkov@gmail.com>
2025-12-10 03:21:28 +0200

    Adjusted structure for app layer and tests

--- src/Eclipse.Application.Contracts/Reports/IReportsService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Eclipse.Application.Contracts.Reports;
-
-public interface IReportsService
-{
-    Task<MemoryStream> GetMoodReportAsync(Guid userId, MoodReportOptions options, CancellationToken cancellationToken = default);
-}
