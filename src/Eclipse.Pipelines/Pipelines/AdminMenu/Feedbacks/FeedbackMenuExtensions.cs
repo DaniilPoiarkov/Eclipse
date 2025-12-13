381546cd68cdfcdc1f1b9de5a1d80d95935dfa2a
unknown <daniilpoyarkov@gmail.com>
2025-12-13 21:18:37 +0200

    Extended pipeline and localized message

---
+++ src/Eclipse.Pipelines/Pipelines/AdminMenu/Feedbacks/FeedbackMenuExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Eclipse.Application.Contracts.Feedbacks;
+using Eclipse.Application.Contracts.Users;
+
+namespace Eclipse.Pipelines.Pipelines.AdminMenu.Feedbacks;
+
+internal static class FeedbackMenuExtensions
+{
+    internal static string GetDisplayName(this UserSlimDto user)
+    {
+        if (!user.UserName.IsNullOrEmpty())
+        {
+            return $"@{user.UserName} {user.ChatId}";
+        }
+
+        return $"{user.Name} {user.Surname} {user.ChatId}";
+    }
+
+    internal static string GetReportingView(this FeedbackDto feedback)
+    {
+        if (!feedback.Comment.IsNullOrEmpty())
+        {
+            return $"{feedback.Rate} | {feedback.Comment}";
+        }
+
+        return $"{feedback.Rate}";
+    }
+}
