1fdadfc4a77a33fe0d7f03e0baf632edfd650685
unknown <daniilpoyarkov@gmail.com>
2025-12-16 16:15:01 +0200

    Enhanced structure and added sender

---
+++ src/Eclipse.Pipelines/Reminders/ReminderSender.cs
@@ -0,0 +1,54 @@
+﻿using Eclipse.Application.Reminders.Sendings;
+using Eclipse.Localization.Culture;
+
+using Microsoft.Extensions.Localization;
+
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Eclipse.Pipelines.Reminders;
+
+internal sealed class ReminderSender : IReminderSender
+{
+    private readonly ITelegramBotClient _client;
+
+    private readonly ICurrentCulture _currentCulture;
+
+    private readonly IStringLocalizer<ReminderSender> _localizer;
+
+    public ReminderSender(
+        ITelegramBotClient client,
+        ICurrentCulture currentCulture,
+        IStringLocalizer<ReminderSender> localizer)
+    {
+        _client = client;
+        _currentCulture = currentCulture;
+        _localizer = localizer;
+    }
+
+    public async Task Send(ReminderArguments arguments, CancellationToken cancellationToken = default)
+    {
+        using var _ = _currentCulture.UsingCulture(arguments.Culture);
+
+        if (!arguments.RelatedItemId.HasValue)
+        {
+            await _client.SendMessage(
+                arguments.ChatId,
+                $"{_localizer["Jobs:SendReminders:Message"]}\n\r\n\r{arguments.Text}",
+                cancellationToken: cancellationToken
+            );
+
+            return;
+        }
+
+        // TODO: Start pipeline.
+        var message = $"{_localizer["Jobs:SendReminders:Message"]}\n\r\n\r{arguments.Text}";
+
+        // TODO: Localize button texts, start according pipeline
+        List<InlineKeyboardButton> menu = arguments.RelatedItemId.HasValue
+            ? [InlineKeyboardButton.WithCallbackData("Finish", arguments.RelatedItemId.Value.ToString()), InlineKeyboardButton.WithCallbackData("Reschedule", "reschedule")]
+            : [];
+
+        await _client.SendMessage(arguments.ChatId, message, replyMarkup: menu, cancellationToken: cancellationToken);
+    }
+}
