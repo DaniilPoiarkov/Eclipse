6af52ad14083696e12923d5e43f6ccb58faea35b
unknown <daniilpoyarkov@gmail.com>
2025-12-16 22:40:45 +0200

    Implemented sender and strategies

--- src/Eclipse.Pipelines/Reminders/ReminderSender.cs
+++ src/Eclipse.Pipelines/Reminders/ReminderSender.cs
@@ -1,54 +1,24 @@
 ﻿using Eclipse.Application.Reminders.Sendings;
-using Eclipse.Localization.Culture;
 
-using Microsoft.Extensions.Localization;
-
-using Telegram.Bot;
-using Telegram.Bot.Types.ReplyMarkups;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Eclipse.Pipelines.Reminders;
 
 internal sealed class ReminderSender : IReminderSender
 {
-    private readonly ITelegramBotClient _client;
+    private readonly IServiceProvider _serviceProvider;
 
-    private readonly ICurrentCulture _currentCulture;
-
-    private readonly IStringLocalizer<ReminderSender> _localizer;
-
-    public ReminderSender(
-        ITelegramBotClient client,
-        ICurrentCulture currentCulture,
-        IStringLocalizer<ReminderSender> localizer)
+    public ReminderSender(IServiceProvider serviceProvider)
     {
-        _client = client;
-        _currentCulture = currentCulture;
-        _localizer = localizer;
+        _serviceProvider = serviceProvider;
     }
 
     public async Task Send(ReminderArguments arguments, CancellationToken cancellationToken = default)
     {
-        using var _ = _currentCulture.UsingCulture(arguments.Culture);
+        IReminderSenderStrategy strategy = arguments.RelatedItemId.HasValue
+            ? _serviceProvider.GetRequiredService<HasRelatedItemReminderStrategy>()
+            : _serviceProvider.GetRequiredService<SingleTimeReminderStrategy>();
 
-        if (!arguments.RelatedItemId.HasValue)
-        {
-            await _client.SendMessage(
-                arguments.ChatId,
-                $"{_localizer["Jobs:SendReminders:Message"]}\n\r\n\r{arguments.Text}",
-                cancellationToken: cancellationToken
-            );
-
-            return;
-        }
-
-        // TODO: Start pipeline.
-        var message = $"{_localizer["Jobs:SendReminders:Message"]}\n\r\n\r{arguments.Text}";
-
-        // TODO: Localize button texts, start according pipeline
-        List<InlineKeyboardButton> menu = arguments.RelatedItemId.HasValue
-            ? [InlineKeyboardButton.WithCallbackData("Finish", arguments.RelatedItemId.Value.ToString()), InlineKeyboardButton.WithCallbackData("Reschedule", "reschedule")]
-            : [];
-
-        await _client.SendMessage(arguments.ChatId, message, replyMarkup: menu, cancellationToken: cancellationToken);
+        await strategy.Send(arguments, cancellationToken);
     }
 }
