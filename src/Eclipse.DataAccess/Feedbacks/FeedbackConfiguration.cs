2255d33b90b03c410362c24e0c2c5be2ffbcb849
unknown <daniilpoyarkov@gmail.com>
2025-12-26 23:20:23 +0200

    Refactored entity configurations

--- src/Eclipse.DataAccess/Feedbacks/FeedbackConfiguration.cs
+++ src/Eclipse.DataAccess/Feedbacks/FeedbackConfiguration.cs
@@ -1,25 +1,15 @@
-﻿using Eclipse.DataAccess.Cosmos;
-using Eclipse.Domain.Feedbacks;
+﻿using Eclipse.Domain.Feedbacks;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.Extensions.Options;
 
 namespace Eclipse.DataAccess.Feedbacks;
 
 internal sealed class FeedbackConfiguration : IEntityTypeConfiguration<Feedback>
 {
-    private readonly IOptions<CosmosDbContextOptions> _options;
-
-    public FeedbackConfiguration(IOptions<CosmosDbContextOptions> options)
-    {
-        _options = options;
-    }
-
     public void Configure(EntityTypeBuilder<Feedback> builder)
     {
-        builder.ToContainer(_options.Value.Container)
-            .HasPartitionKey(u => u.Id);
+        builder.HasPartitionKey(u => u.Id);
 
         builder.HasDiscriminator<string>("Discriminator")
             .HasValue(nameof(Feedback));
