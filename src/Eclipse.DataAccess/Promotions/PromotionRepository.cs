02b0635e3da0c59cc8d2a55d4d7212c983425126
unknown <daniilpoyarkov@gmail.com>
2025-12-26 23:20:07 +0200

    Added repo implementation

---
+++ src/Eclipse.DataAccess/Promotions/PromotionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Eclipse.DataAccess.Cosmos;
+using Eclipse.DataAccess.Repositories;
+using Eclipse.Domain.Promotions;
+
+namespace Eclipse.DataAccess.Promotions;
+
+internal sealed class PromotionRepository : RepositoryBase<Promotion>, IPromotionRepository
+{
+    public PromotionRepository(EclipseDbContext context)
+        : base(context) { }
+}
