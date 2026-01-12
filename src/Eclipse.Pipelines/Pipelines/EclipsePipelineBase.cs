3014fa462611acd4afe3419b8b5719c190bf6215
unknown <daniilpoyarkov@gmail.com>
2026-01-12 02:08:01 +0200

    Initial pipeline setup

--- src/Eclipse.Pipelines/Pipelines/EclipsePipelineBase.cs
+++ src/Eclipse.Pipelines/Pipelines/EclipsePipelineBase.cs
@@ -1,8 +1,13 @@
-﻿using Eclipse.Core.Pipelines;
+﻿using Eclipse.Common.Linq;
+using Eclipse.Core.Pipelines;
 using Eclipse.Core.Results;
+using Eclipse.Domain.Promotions;
 
 using Microsoft.Extensions.Localization;
 
+using System.Collections.Generic;
+
+using Telegram.Bot.Requests.Abstractions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 
@@ -39,9 +44,21 @@ public abstract class EclipsePipelineBase : PipelineBase
 
     protected IResult MenuAndClearPrevious(IEnumerable<IEnumerable<KeyboardButton>> buttons, Message? message, string text)
     {
+        return MenuAndClearPrevious(
+            new ReplyKeyboardMarkup(buttons)
+            {
+                ResizeKeyboard = true
+            },
+            message,
+            text
+        );
+    }
+
+    protected IResult MenuAndClearPrevious(ReplyMarkup buttons, Message? message, string text)
+    {
         var menu = Menu(buttons, text);
 
-        if (message is null)
+        if (message is not { ReplyMarkup: { } })
         {
             return menu;
         }
@@ -49,5 +66,36 @@ public abstract class EclipsePipelineBase : PipelineBase
         var edit = Edit(message.MessageId, InlineKeyboardMarkup.Empty());
 
         return Multiple(menu, edit);
+    }
+
+    protected List<IList<InlineKeyboardButton>> GetPagingButtons<T>(int page, PaginatedList<T> list)
+    {
+        List<IList<InlineKeyboardButton>> buttons = [];
+
+        if (page > 1 && page < list.Pages)
+        {
+            buttons.Add([InlineKeyboardButton.WithCallbackData("◀️"), InlineKeyboardButton.WithCallbackData("▶️")]);
+            buttons.Add([InlineKeyboardButton.WithCallbackData(Localizer["GoBack"], "go_back")]);
+        }
+        if (page == 1 && list.Pages > 1)
+        {
+            buttons.Add([InlineKeyboardButton.WithCallbackData("▶️")]);
+            buttons.Add([InlineKeyboardButton.WithCallbackData(Localizer["GoBack"], "go_back")]);
+        }
+        if (page == list.Pages && list.Pages > 1)
+        {
+            buttons.Add([InlineKeyboardButton.WithCallbackData("◀️")]);
+            buttons.Add([InlineKeyboardButton.WithCallbackData(Localizer["GoBack"], "go_back")]);
+        }
+
+        return buttons;
+    }
+
+    protected string GetPagingMessage(int page, int pagesCount, int totalCount, string message)
+    {
+        return Localizer["PagingInformation{Page}{PagesCount}{TotalCount}", page, pagesCount, totalCount]
+            + Environment.NewLine
+            + Environment.NewLine
+            + message;
     }
 }
