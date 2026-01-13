edc34f0fed0173da55f975310aa0eb021433e9f9
unknown <daniilpoyarkov@gmail.com>
2026-01-13 20:47:04 +0200

    Reworked promtion create pipeline

---
+++ src/Eclipse.Pipelines/Pipelines/AdminMenu/Promotions/CreatePromotionPostPipeline.cs
@@ -0,0 +1,112 @@
+﻿using Eclipse.Application.Contracts.Promotions;
+using Eclipse.Common.Caching;
+using Eclipse.Core.Context;
+using Eclipse.Core.Results;
+using Eclipse.Core.Routing;
+using Eclipse.Pipelines.Stores.Messages;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Eclipse.Pipelines.Pipelines.AdminMenu.Promotions;
+
+[Route("Menu:AdminMenu:Promotions:Create", "/admin_promotions_create")]
+internal sealed class CreatePromotionPostPipeline : AdminPipelineBase
+{
+    private readonly ICacheService _cacheService;
+
+    private readonly ITelegramBotClient _botClient;
+
+    private readonly IPromotionService _promotionService;
+
+    private readonly IMessageStore _messageStore;
+
+    public CreatePromotionPostPipeline(
+        ICacheService cacheService,
+        ITelegramBotClient botClient,
+        IPromotionService promotionService,
+        IMessageStore messageStore)
+    {
+        _cacheService = cacheService;
+        _botClient = botClient;
+        _promotionService = promotionService;
+        _messageStore = messageStore;
+    }
+
+    protected override void Initialize()
+    {
+        RegisterStage(RequestPostMessage);
+        RegisterStage(ReviewPostMessageAsync);
+        RegisterStage(SavePromotionPostAsync);
+    }
+
+    private IResult RequestPostMessage(MessageContext context)
+    {
+        return Menu(new ReplyKeyboardRemove(), Localizer["Pipelines:Admin:Promotions:Create:Request"]);
+    }
+
+    private async Task<IResult> ReviewPostMessageAsync(MessageContext context, CancellationToken cancellationToken)
+    {
+        if (Update.Message is not { })
+        {
+            FinishPipeline();
+            return Menu(PromotionsButtons, Localizer["Pipelines:Admin:Promotions:Create:Invalid"]);
+        }
+
+        var options = new CacheOptions
+        {
+            Expiration = CacheConsts.FiveMinutes
+        };
+
+        await _cacheService.SetAsync(
+            $"promotions-create-message-{context.ChatId}",
+            Update.Message.Id,
+            options,
+            cancellationToken
+        );
+
+        await _botClient.CopyMessage(context.ChatId, context.ChatId, Update.Message.Id, cancellationToken: cancellationToken);
+
+        return Menu(
+            [
+                [ new KeyboardButton(Localizer["Pipelines:Admin:Promotions:Create:Review:Continue"]) ],
+                [ new KeyboardButton(Localizer["Pipelines:Admin:Promotions:Create:Review:Cancel"]) ]
+            ],
+            Localizer["Pipelines:Admin:Promotions:Create:Review"]
+        );
+    }
+
+    private async Task<IResult> SavePromotionPostAsync(MessageContext context, CancellationToken cancellationToken)
+    {
+        var message = await _messageStore.GetOrDefaultAsync(
+            new MessageKey(context.ChatId),
+            cancellationToken
+        );
+
+        if (context.Value == "go_back")
+        {
+            return MenuAndClearPrevious(PromotionsButtons, message, Localizer["Okay"]);
+        }
+
+        var messageId = await _cacheService.GetOrCreateAsync(
+            $"promotions-create-message-{context.ChatId}",
+            () => Task.FromResult<int?>(null),
+            cancellationToken: cancellationToken
+        );
+
+        if (!messageId.HasValue)
+        {
+            return Menu(PromotionsButtons, Localizer["Pipelines:Admin:Promotions:Create:MessageNotFound"]);
+        }
+
+        var promotion = await _promotionService.Create(
+            new CreatePromotionRequest(context.ChatId, messageId.GetValueOrDefault(), string.Empty),
+            cancellationToken
+        );
+
+        return Redirect<PublishPromotionPipeline>(
+            Menu(new ReplyKeyboardRemove(), Localizer["Pipelines:Admin:Promotions:Create:Success"])
+        );
+    }
+}
