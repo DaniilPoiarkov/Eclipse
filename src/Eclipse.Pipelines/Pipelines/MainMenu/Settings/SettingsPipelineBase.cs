eb786768caff2eafe4ad7a51687a28391f66ea79
Daniil <daniilpoyarkov@gmail.com>
2026-06-07 19:42:06 +0300

    Added menu

--- src/Eclipse.Pipelines/Pipelines/MainMenu/Settings/SettingsPipelineBase.cs
+++ src/Eclipse.Pipelines/Pipelines/MainMenu/Settings/SettingsPipelineBase.cs
@@ -7,6 +7,7 @@ public abstract class SettingsPipelineBase : EclipsePipelineBase
     protected IEnumerable<IEnumerable<KeyboardButton>> SettingsMenuButtons => new List<IList<KeyboardButton>>
     {
         new[] { new KeyboardButton(Localizer["Menu:Settings:Language"]), new KeyboardButton(Localizer["Menu:Settings:SetGmt"]) },
+        new[] { new KeyboardButton(Localizer["Menu:ApiTokens"]) },
         new[] { new KeyboardButton(Localizer["Menu:MainMenu"]) }
     };
 }
