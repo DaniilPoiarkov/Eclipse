9f9fbf97bfded538ef09df6a9d2f8e63f648dcd8
Daniil <daniilpoyarkov@gmail.com>
2026-06-07 23:35:53 +0300

    Improved messaging

--- src/Eclipse.Pipelines/Pipelines/MainMenu/ApiTokens/ApiTokensPipeline.cs
+++ src/Eclipse.Pipelines/Pipelines/MainMenu/ApiTokens/ApiTokensPipeline.cs
@@ -1,12 +1,38 @@
+using Eclipse.Core.Context;
+using Eclipse.Core.Results;
 using Eclipse.Core.Routing;
+
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Eclipse.Pipelines.Pipelines.MainMenu.ApiTokens;
 
 [Route("Menu:ApiTokens", "/api_tokens")]
 internal sealed class ApiTokensPipeline : ApiTokensPipelineBase
 {
+    private readonly ITelegramBotClient _client;
+
+    public ApiTokensPipeline(ITelegramBotClient client)
+    {
+        _client = client;
+    }
+
     protected override void Initialize()
     {
-        RegisterStage(_ => Menu(ApiTokensMenuButtons, Localizer["Pipelines:ApiTokens"]));
+        RegisterStage(SendApiTokensInfo);
+    }
+
+    private async Task<IResult> SendApiTokensInfo(MessageContext context, CancellationToken cancellationToken)
+    {
+        await _client.SendMessage(
+            chatId: context.ChatId,
+            text: Localizer["Pipelines:ApiTokens"],
+            replyMarkup: new ReplyKeyboardMarkup(ApiTokensMenuButtons) { ResizeKeyboard = true },
+            parseMode: ParseMode.Markdown,
+            cancellationToken: cancellationToken
+        );
+
+        return Empty();
     }
 }
